95bb0e59c08726f2caee832810e8683b451c15f7
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2020-02-26 14:59:06 +0100

    cleaning

--- src/core/distributions/phylogenetics/tree/birthdeath/AbstractBirthDeathProcess.h
+++ src/core/distributions/phylogenetics/tree/birthdeath/AbstractBirthDeathProcess.h
@@ -18,15 +18,15 @@ namespace RevBayesCore {
         
     protected:
         // pure virtual helper functions
-        virtual double                                      computeLnProbabilityTimes(void) const = 0;  //!< Compute the log-transformed probability of the current value.
+        virtual double                                      computeLnProbabilityTimes(void) const = 0;                                              //!< Compute the log-transformed probability of the current value.
         virtual size_t                                      getNumberOfTaxaAtPresent(void) const;
-        virtual double                                      lnProbNumTaxa(size_t n, double start, double end, bool MRCA) const = 0;     //!< Compute the log-transformed probability of the number of taxa.
-        virtual double                                      lnProbTreeShape(void) const;    //!< Compute the log-transformed probability of the tree shape.
-        virtual double                                      pSurvival(double start, double end) const = 0;  //!< Compute the probability of survival of the process.
-        virtual void                                        prepareProbComputation(void) const;     //!< Prepare likelihood calculation
+        virtual double                                      lnProbNumTaxa(size_t n, double start, double end, bool MRCA) const = 0;                 //!< Compute the log-transformed probability of the number of taxa.
+        virtual double                                      lnProbTreeShape(void) const;                                                            //!< Compute the log-transformed probability of the tree shape.
+        virtual double                                      pSurvival(double start, double end) const = 0;                                          //!< Compute the probability of survival of the process.
+        virtual void                                        prepareProbComputation(void) const;                                                     //!< Prepare likelihood calculation
         
         // helper functions
-        double                                              computeLnProbabilityDivergenceTimes(void) const;    //!< Compute the log-transformed probability of divergence times.
+        double                                              computeLnProbabilityDivergenceTimes(void) const;                                        //!< Compute the log-transformed probability of divergence times.
         virtual std::vector<double>                         simulateDivergenceTimes(size_t n, double origin, double present, double min) const;     //!< Simulate n speciation events.
         
         // members
