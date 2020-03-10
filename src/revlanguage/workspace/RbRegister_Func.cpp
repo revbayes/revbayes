588388808142c62d48b6ff89f2273069cc0cf7f1
Walker Pett <will.pett@gmail.com>
2020-03-10 17:09:19 -0500

    adding simplex sum function

--- src/revlanguage/workspace/RbRegister_Func.cpp
+++ src/revlanguage/workspace/RbRegister_Func.cpp
@@ -227,6 +227,7 @@
 #include "Func_sumPositive.h"
 #include "Func_sumInteger.h"
 #include "Func_sumNatural.h"
+#include "Func_sumSimplex.h"
 #include "Func_standardDeviation.h"
 #include "Func_sqrt.h"
 #include "Func_trunc.h"
@@ -453,6 +454,7 @@ void RevLanguage::Workspace::initializeFuncGlobalWorkspace(void)
         addFunction( new Func_sumPositive()  );
         addFunction( new Func_sumInteger()  );
         addFunction( new Func_sumNatural()  );
+        addFunction( new Func_sumSimplex()  );
 
         // standard deviation function
         addFunction( new Func_standardDeviation()  );
