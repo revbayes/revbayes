3791823bce8982295f205b484cc3842a29f01984
Jeremy-Andreoletti <jeremy.andreoletti@gmail.com>
2020-05-13 16:12:27 +0200

    Verbose option for InferAncestralPopSizeFunction

--- src/core/functions/phylogenetics/InferAncestralPopSizeFunction.cpp
+++ src/core/functions/phylogenetics/InferAncestralPopSizeFunction.cpp
@@ -122,6 +122,8 @@ void InferAncestralPopSizeFunction::update( void )
     const Tree tree = timeTree->getValue();
     const std::vector<double> occurrence_ages = occurrences->getValue();
 
+    std::cout << verbose << std::endl;
+
 	MatrixReal B_Lt_log = RevBayesCore::ComputeLnProbabilitiesBackwardsLt(start_age, lambda, mu, psi, omega, rho, removalPr, maxHiddenLin, cond, time_points, useOrigin, verbose, occurrence_ages, tree);
     bool returnLogLikelihood = false;
 	MatrixReal B_Mt_log = RevBayesCore::ComputeLnProbabilitiesForwardsMt(start_age, lambda, mu, psi, omega, rho, removalPr, maxHiddenLin, cond, time_points, useOrigin, returnLogLikelihood, verbose, occurrence_ages, tree);
