2e794970c4781794a194060a3cb1796ab05c8ec8
Michael Landis <mlandis@gmail.com>
2020-07-07 14:35:07 -0500

    add geometric mean function

---
+++ src/core/functions/builtin/GeometricMeanFunction.hpp
@@ -0,0 +1,14 @@
+//
+//  GeometricMeanFunction.hpp
+//  revbayes-proj
+//
+//  Created by Michael Landis on 7/6/20.
+//  Copyright © 2020 Michael Landis. All rights reserved.
+//
+
+#ifndef GeometricMeanFunction_hpp
+#define GeometricMeanFunction_hpp
+
+#include <stdio.h>
+
+#endif /* GeometricMeanFunction_hpp */
