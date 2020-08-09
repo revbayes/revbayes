d4029438504b2042cd15631df8fda51c31f226b2
Michael Landis <mlandis@gmail.com>
2020-08-08 19:47:48 -0500

    add files for new rate mtx function

--- src/core/datatypes/phylogenetics/ratematrix/RateMatrix_Biogeography.h
+++ src/core/datatypes/phylogenetics/ratematrix/RateMatrix_Biogeography.h
@@ -1,14 +1,95 @@
-//
-//  RateMatrix_Biogeography.hpp
-//  revbayes-tensorphylo-proj
-//
-//  Created by Michael Landis on 8/8/20.
-//  Copyright © 2020 Michael Landis. All rights reserved.
-//
+#ifndef RateMatrix_Biogeography_H
+#define RateMatrix_Biogeography_H
 
-#ifndef RateMatrix_Biogeography_hpp
-#define RateMatrix_Biogeography_hpp
+#include "GeneralRateMatrix.h"
+#include "TransitionProbabilityMatrix.h"
+#include <complex>
+#include <vector>
+#include <map>
+#include <list>
 
-#include <stdio.h>
 
-#endif /* RateMatrix_Biogeography_hpp */
+namespace RevBayesCore {
+    
+    class TransitionProbabilityMatrix;
+    
+    class RateMatrix_Biogeography : public GeneralRateMatrix {
+        
+    public:
+        RateMatrix_Biogeography(size_t ns, size_t nc, size_t mrs);                                                                                               //!< Construct rate matrix with n states
+        RateMatrix_Biogeography(const RateMatrix_Biogeography& m);                                                                                //!< Copy constructor
+        virtual                         ~RateMatrix_Biogeography(void);                                                              //!< Destructor
+        
+        // overloaded operators
+        RateMatrix_Biogeography&                   operator=(const RateMatrix_Biogeography& r);
+        virtual RateMatrix_Biogeography&           assign(const Assignable &m);
+        
+        // RateMatrix functions
+        double                              averageRate(void) const;
+        void                                calculateTransitionProbabilities(double startAge, double endAge, double rate, TransitionProbabilityMatrix& P) const;   //!< Calculate the transition matrix
+        RateMatrix_Biogeography*            clone(void) const;
+        void                                fillRateMatrix(void);
+        const RbVector<RbVector<double> >&  getDispersalRates(void) const;
+        const RbVector<double>&             getExtirpationRates(void) const;                                                   //!< Return the extirpation rates
+        virtual std::vector<double>         getStationaryFrequencies(void) const;                                              //!< Return the stationary frequencies
+
+        void                                setDispersalRates(const RbVector<RbVector<double> >& dr);                          //!< Directly set dispersal rates
+        void                                setExtirpationRates(const RbVector<double>& er);                        //!< Directly set extirpation rates
+        
+//        void                                setBirthRate(const double& br);
+//        void                                setCladogeneticMatrix(const MatrixReal& cp);
+
+        void                                update(void);
+        
+    private:
+        std::string                         getRangeStr(const std::vector<unsigned>& v);
+        void                                calculateCijk(void);                                                                //!< Do precalculations on
+//        void                                computeConditionSurvival(MatrixReal& r);
+//        void                                computeConditionSurvival(TransitionProbabilityMatrix& r);
+        void                                tiProbsEigens(double t, TransitionProbabilityMatrix& P) const;                      //!< Calculate transition probabilities for real case
+        void                                tiProbsComplexEigens(double t, TransitionProbabilityMatrix& P) const;               //!< Calculate transition probabilities for complex case
+        void                                updateEigenSystem(void);                                                            //!< Update the system of eigenvalues and eigenvectors
+        
+        
+        size_t                                              numBitsOn(std::vector<unsigned> v);
+        void                                                makeBits(void);
+        void                                                makeTransitions(void);
+        
+        std::vector<std::vector<unsigned> >                 bits;
+        std::map<std::vector<unsigned>, unsigned>           inverseBits;
+        std::vector<std::vector<std::vector<unsigned> > >   bitsByNumOn;
+        std::vector<std::vector<unsigned> >                 statesToBitsByNumOn;
+        std::map< std::vector<unsigned>, unsigned>          bitsToStatesByNumOn;
+        std::vector<std::vector<unsigned> >                 transitions;
+        std::vector<std::vector<unsigned> >                 lossOrGain;
+        std::vector<std::vector<std::vector<unsigned> > >   transitionAreas;
+        std::vector<std::vector<unsigned> >                 changedAreas;
+        std::vector<std::vector<std::vector<unsigned> > >   affectingAreas;
+        size_t                                              numCharacters;
+        size_t                                              num_states;
+        bool                                                useSquaring;
+        
+        EigenSystem*                                        theEigenSystem;                 //!< Holds the eigen system
+        std::vector<double>                                 c_ijk;                          //!< Vector of precalculated product of eigenvectors and their inverse
+        std::vector<std::complex<double> >                  cc_ijk;                         //!< Vector of precalculated product of eigenvectors and thier inverse for complex case
+        
+        // members
+        RbVector<RbVector<double> >                         dispersalRates;
+        RbVector<double>                                    extirpationRates;
+        MatrixReal                                          cladogeneticMatrix;
+        double                                              scalingFactor;
+        double                                              birthRate;
+        bool                                                useCladogenesis;
+        bool                                                rescaleMatrix;
+        size_t                                              maxRangeSize;
+        TransitionProbabilityMatrix                         stationaryMatrix;
+        
+        mutable std::map<double,TransitionProbabilityMatrix>  storedTransitionProbabilities;
+        mutable std::list<double>                             accessedTransitionProbabilities;
+        unsigned                                              maxSizeStoredTransitionProbabilites;
+        bool                                                  useStoredTransitionProbabilities;
+    };
+    
+}
+
+#endif /* defined(__revbayes_proj__RateMatrix_Biogeography__) */
