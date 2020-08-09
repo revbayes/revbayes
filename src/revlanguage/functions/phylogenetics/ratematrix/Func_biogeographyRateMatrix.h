4f8c6da59c556038c36e2656b9a772cc7ada73f6
Michael Landis <mlandis@gmail.com>
2020-08-08 19:45:40 -0500

    add rate matrix function for new state space

---
+++ src/revlanguage/functions/phylogenetics/ratematrix/Func_biogeographyRateMatrix.h
@@ -0,0 +1,14 @@
+//
+//  Func_BiogeographyRateMatrix.hpp
+//  revbayes-tensorphylo-proj
+//
+//  Created by Michael Landis on 8/8/20.
+//  Copyright © 2020 Michael Landis. All rights reserved.
+//
+
+#ifndef Func_BiogeographyRateMatrix_hpp
+#define Func_BiogeographyRateMatrix_hpp
+
+#include <stdio.h>
+
+#endif /* Func_BiogeographyRateMatrix_hpp */
