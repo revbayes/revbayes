a55106f0af39dd5f1fd3aa36282d391d1d44e839
ahsiang <allison.hsiang@gmail.com>
2020-08-18 13:01:45 +0200

    Adding TreeVector datatype

---
+++ src/core/datatypes/trees/TreeVector.h
@@ -0,0 +1,56 @@
+#ifndef TreeVector_h
+#define TreeVector_h
+
+#include <stdio.h>
+#include <vector>
+#include <iosfwd>
+
+#include "Tree.h"
+#include "TypedDagNode.h"
+
+namespace RevBayesCore {
+
+
+    /**
+     * @brief Vector of trees
+     *
+     *
+     * @copyright Copyright 2009-
+     * @author
+     * @since 2014-03-18, version 1.0
+     */
+    class TreeVector {
+
+    public:
+
+        TreeVector(void);                                              //!< Default constructor
+        virtual                             ~TreeVector() {}
+
+        bool                                operator==(const TreeVector &mve) const;
+        bool                                operator!=(const TreeVector &mve) const;
+
+        // public methods
+        void                                addValues(const Tree *t, long n);
+        void                                clear(void);
+        TreeVector*                         clone(void) const;
+        size_t                              getNumberOfValues(void) const;
+        std::vector<Tree>&                  getValues(void);                                                //!< Get the values for this element.
+        const std::vector<Tree>&            getValues(void) const;                                          //!< Get the values for this element.
+        void                                setNumberOfTrees(long n);
+        void                                setValues(const std::vector<Tree> &v, const long n);
+
+
+    private:
+
+        // private members
+        long                                num_trees;
+        std::vector<Tree>                   values;
+
+    };
+
+    // Global functions using the class
+    std::ostream&                       operator<<(std::ostream& o, const TreeVector& x);                                         //!< Overloaded output operator
+}
+
+
+#endif /* TreeVector_hpp */
