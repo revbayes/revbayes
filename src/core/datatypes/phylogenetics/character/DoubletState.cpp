630f909bdb265fee57632373aa8a325c17cffabb
bjoelle <joellebs@iastate.edu>
2020-12-17 11:23:44 -0600

    Typo in error message

--- src/core/datatypes/phylogenetics/character/DoubletState.cpp
+++ src/core/datatypes/phylogenetics/character/DoubletState.cpp
@@ -100,7 +100,7 @@ std::string DoubletState::getStringValue(void) const
     if (tmp.getState() == state)
         return str_val;
 
-    throw RbException("This ambiguous doublet character (which looks like of like "+str_val+") is not representable as a two-letter code");
+    throw RbException("This ambiguous doublet character (which looks like "+str_val+") is not representable as a two-letter code");
 }
 
 
