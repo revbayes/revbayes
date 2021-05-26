a850eaab048368a5b63aaeb2f70e1e9b4d195c4b
Walker Pett <will.pett@gmail.com>
2021-05-26 11:07:13 -0500

    update taxon reader for minmin/minmax and maxmin/maxmax

--- src/core/io/TaxonReader.cpp
+++ src/core/io/TaxonReader.cpp
@@ -30,7 +30,7 @@ TaxonReader::TaxonReader(const std::string &fn, char delim) : DelimitedDataReade
     std::vector<std::string>& line = chars[0];
     std::map<std::string, int> column_map;
 
-    std::string arr[] = {"taxon","age","species","min","max"};
+    std::string arr[] = {"taxon","age","species","min","max", "minmin", "minmax", "maxmin", "maxmax"};
     std::vector<std::string> fields (arr, arr + sizeof(arr) / sizeof(arr[0]) );
     
     for (size_t i = 0 ; i < line.size() ; ++i)
@@ -58,19 +58,47 @@ TaxonReader::TaxonReader(const std::string &fn, char delim) : DelimitedDataReade
     
     if (column_map.find("taxon") == column_map.end())
     {
-        throw RbException("Missing header in the taxon definition file. It has to contain \"taxon\" field.");
+        throw RbException("Taxon definition file must contain \"taxon\" field.");
     }
     
+    std::map<std::string,int>::iterator speciesit = column_map.find("species");
+    std::map<std::string,int>::iterator ageit = column_map.find("age");
     std::map<std::string,int>::iterator minit = column_map.find("min");
     std::map<std::string,int>::iterator maxit = column_map.find("max");
+    std::map<std::string,int>::iterator minminit = column_map.find("minmin");
+    std::map<std::string,int>::iterator minmaxit = column_map.find("minmax");
+    std::map<std::string,int>::iterator maxminit = column_map.find("maxmin");
+    std::map<std::string,int>::iterator maxmaxit = column_map.find("maxmax");
 
-    if ( (minit == column_map.end() || maxit == column_map.end()) && minit != maxit)
+    if ( (minit != column_map.end() || minminit != column_map.end() || minmaxit != column_map.end())
+            && maxit == column_map.end() && maxminit == column_map.end() && maxmaxit == column_map.end())
     {
-        throw RbException("Taxon definition file header must contain both \"min\" and \"max\" age fields");
+        throw RbException("Taxon definition file header containing a \"min*\" age field must also contain a \"max*\" age field");
     }
-    if ( (minit != column_map.end() || maxit != column_map.end()) && column_map.find("age") != column_map.end())
+    if ( (maxit != column_map.end() || maxminit != column_map.end() || maxmaxit != column_map.end())
+            && minit == column_map.end() && minminit == column_map.end() && minmaxit == column_map.end())
     {
-        throw RbException("Taxon definition file header cannot contain both \"age\" and (\"min\" or \"max\") fields");
+        throw RbException("Taxon definition file header containing a \"max*\" age field must also contain a \"min*\" age field");
+    }
+    if ( (minminit == column_map.end() || minmaxit == column_map.end()) && minminit != minmaxit)
+    {
+        throw RbException("Taxon definition file header must contain both \"minmin\" and \"minmax\" age fields");
+    }
+    if ( (maxminit == column_map.end() || maxmaxit == column_map.end()) && maxminit != maxmaxit)
+    {
+        throw RbException("Taxon definition file header must contain both \"maxmin\" and \"maxmax\" age fields");
+    }
+    if ( (minit != column_map.end() || maxit != column_map.end()
+            || minminit != column_map.end() || minmaxit != column_map.end()
+            || maxminit != column_map.end() || maxmaxit != column_map.end()) && ageit != column_map.end())
+    {
+        throw RbException("Taxon definition file header cannot contain both \"age\" and (\"min*\" or \"max*\") fields");
+    }
+    if ( (minit != column_map.end() || maxit != column_map.end()) &&
+            (minminit != column_map.end() || minmaxit != column_map.end()
+            || maxminit != column_map.end() || maxmaxit != column_map.end()))
+    {
+        throw RbException("Taxon definition file header cannot contain both \"min/max\" and (\"min/maxmin\" or \"min/maxmax\") fields");
     }
 
     for (size_t i = 1; i < chars.size(); ++i) //going through all the lines
@@ -83,25 +111,9 @@ TaxonReader::TaxonReader(const std::string &fn, char delim) : DelimitedDataReade
             throw RbException(err.str());
         }
         Taxon t = Taxon( line[ column_map["taxon"] ] );
+        t.setAge( 0.0 );
         
-        if ( column_map.find("min") != column_map.end() )
-        {
-            double min = 0,max = 0;
-            TimeInterval interval;
-            std::stringstream ss;
-
-            ss.str( line[ column_map["min"] ] );
-            ss >> min;
-            interval.setMin(min);
-
-            ss.clear();
-            ss.str( line[ column_map["max"] ] );
-            ss >> max;
-            interval.setMax(max);
-
-            t.setAgeRange(interval);
-        }
-        else if ( column_map.find("age") != column_map.end() )
+        if ( ageit != column_map.end() )
         {
             double age = 0.0;
             std::stringstream ss;
@@ -109,13 +121,65 @@ TaxonReader::TaxonReader(const std::string &fn, char delim) : DelimitedDataReade
             ss >> age;
             t.setAge( age );
         }
-        else
+
+        if ( minit != column_map.end() || minminit != column_map.end() )
         {
-            t.setAge( 0.0 );
+            double age = 0;
+            TimeInterval interval;
+            std::stringstream ss;
+
+            if ( minminit != column_map.end() )
+            {
+                ss.str( line[ column_map["minmin"] ] );
+                ss >> age;
+                interval.setMin(age);
+
+                ss.clear();
+                ss.str( line[ column_map["minmax"] ] );
+                ss >> age;
+                interval.setMax(age);
+            }
+            else if ( minit != column_map.end() )
+            {
+                ss.str( line[ column_map["min"] ] );
+                ss >> age;
+                interval.setMin(age);
+                interval.setMax(age);
+            }
+
+            t.setMinAgeRange(interval);
+        }
+
+        if ( maxit != column_map.end() || maxminit != column_map.end() )
+        {
+            double age = 0;
+            TimeInterval interval;
+            std::stringstream ss;
+
+            if ( maxminit != column_map.end() )
+            {
+                ss.str( line[ column_map["maxmin"] ] );
+                ss >> age;
+                interval.setMin(age);
+
+                ss.clear();
+                ss.str( line[ column_map["maxmax"] ] );
+                ss >> age;
+                interval.setMax(age);
+            }
+            else if ( maxit != column_map.end() )
+            {
+                ss.str( line[ column_map["max"] ] );
+                ss >> age;
+                interval.setMin(age);
+                interval.setMax(age);
+            }
+
+            t.setMaxAgeRange(interval);
         }
 
         
-        if ( column_map.find("species") != column_map.end() )
+        if ( speciesit != column_map.end() )
         {
             t.setSpeciesName( line[ column_map["species"] ] );
         }
