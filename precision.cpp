a741354db0ab2844ce2b2933ab43a1be1b782ca5
Walker Pett <will.pett@gmail.com>
2021-08-07 10:16:03 -0500

    Revert "update precision test"
    This reverts commit aef38dd336c0a416e1b57978f92b6f2f843eabf0.

--- precision.cpp
+++ precision.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+//#pragma GCC target("fpmath=sse,sse2") // Turns off excess precision
+// #pragma GCC target("fpmath=387") // Turns on excess precision
+ 
+#include <iostream>
+#include <cstdlib>
+#include <cfloat>
+using namespace std;
+ 
 int main() {
-  double x = 50178230318.0;
-  double y = 100000000000.0;
-  double ratio = x/y;
-  printf("x/y  = %18.18f\n", ratio);
+  cout << "This is compiled in mode "<< FLT_EVAL_METHOD << '\n';
+  cout << "0 means no excess precision.\n";
+  cout << "2 means there is excess precision.\n\n";
+  
+  cout << "The following test detects excess precision\n";
+  cout << "0 if no excess precision, or 8e-17 if there is excess precision.\n";
+  double a = atof("1.2345678");
+  double b = a*a;
+  cout << b - 1.52415765279683990130 << '\n';
+  return 0;
 }
