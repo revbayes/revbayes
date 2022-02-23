3aa7de9bf78383c019fde8bdffbac3dac509780b
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-02-23 14:28:50 +0100

    making sure the root node has the correct flag for heterochronous coalescent

--- src/core/distributions/phylogenetics/tree/coalescent/AbstractCoalescent.cpp
+++ src/core/distributions/phylogenetics/tree/coalescent/AbstractCoalescent.cpp
@@ -451,7 +451,6 @@ void AbstractCoalescent::simulateHeterochronousTree( void )
         node->setName(name);
         node->setSpeciesName(taxa[i].getSpeciesName());
         node->setAge(taxa[i].getAge());
-        node->setNodeType( true, false, false );
         // add to tips
         nodes.push_back(node);
     }
@@ -465,6 +464,7 @@ void AbstractCoalescent::simulateHeterochronousTree( void )
     // initialize the topology by setting the root
     // the root is the only node left in nodes 
     TopologyNode* root = nodes[0]; // Only node left after coalescing all is the root
+    root->setNodeType(false, true, true); // to be sure that this node is flagged as the root node
     psi->setRoot(root, true);
 
     // finally store the new value
