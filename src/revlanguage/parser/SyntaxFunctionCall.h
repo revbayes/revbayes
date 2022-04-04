d93240f8345eac63b86d983982c38cf8d19fa20f
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-04-04 10:59:00 -0400

    Add member function to push new first argument.

--- src/revlanguage/parser/SyntaxFunctionCall.h
+++ src/revlanguage/parser/SyntaxFunctionCall.h
@@ -79,13 +79,15 @@ namespace RevLanguage {
         SyntaxFunctionCall*                 clone() const;                                                              //!< Clone object
         
         // Regular functions
-        const std::string&                  getFunctionName(void) const { return function_name; }                        //!< Get function name
+        const std::string&                  getFunctionName(void) const { return function_name; }                       //!< Get function name
         RevPtr<RevVariable>                 evaluateContent(Environment& env, bool dynamic=false);                      //!< Get semantic value
-        void                                setBaseVariable(SyntaxElement* var) { base_variable = var; }                 //!< Set base variable
+        void                                setBaseVariable(SyntaxElement* var) { base_variable = var; }                //!< Set base variable
+        void                                pipeAddArg(SyntaxLabeledExpr* arg) { arguments->push_front(arg); }          //!< Add first argument from pipe
         bool                                isConstExpression(void) const;                                              //!< Is this a constant expression?
         bool                                isFunctionSafe(const Environment&       env,
                                                            std::set<std::string>&   localVars) const;                   //!< Is this element safe in a function?
  
+
     protected:
         
         std::list<SyntaxLabeledExpr*>*      arguments;                                                                  //!< The arguments passed to the function
