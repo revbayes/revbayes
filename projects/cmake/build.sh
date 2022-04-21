c49f5649e3dad0ab1fd82eedf9910ca3c2892f9f
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-04-21 13:01:38 -0400

    Remove unused code to run tests.
    This is part of github actions now.

--- projects/cmake/build.sh
+++ projects/cmake/build.sh
@@ -179,17 +179,3 @@ else
     fi
 fi
 
-# Run tests
-if [ "$travis" = "true" ] && [ "${TRAVIS_BUILD_STAGE_NAME}" = "Test" ]
-then
-  cd ../..
-  echo "\"Hello World\"" | projects/cmake/rb
-  cd tests
-  ./run_integration_tests.sh -mpi ${USE_MPI} ${TRAVIS_BUILD_DIR}/projects/cmake/rb
-  # Run testiphy
-  export PATH=${TRAVIS_BUILD_DIR}/projects/cmake:$PATH
-  cd
-  git clone https://gitlab.com/testiphy/testiphy.git
-  cd testiphy
-  ./testiphy rb
-fi
