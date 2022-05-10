77aea36ec99ccce02f37e56c5431cbd184f78119
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-05-10 13:38:48 +0200

    Adding a new PoMo state converter also for binary data

--- src/revlanguage/workspace/RbRegister_Func.cpp
+++ src/revlanguage/workspace/RbRegister_Func.cpp
@@ -102,6 +102,7 @@
 #include "Func_PhylogeneticIndependentContrasts.h"
 #include "Func_PhylogeneticIndependentContrastsMultiSample.h"
 #include "Func_pomoState4Converter.h"
+#include "Func_pomoStateConverter.h"
 #include "Func_pomoRootFrequencies.h"
 #include "Func_pruneTree.h"
 #include "Func_simStartingTree.h"
@@ -420,6 +421,7 @@ void RevLanguage::Workspace::initializeFuncGlobalWorkspace(void)
         addFunction( new Func_PhylogeneticIndependentContrasts()                );
         addFunction( new Func_PhylogeneticIndependentContrastsMultiSample()     );
         addFunction( new Func_pomoState4Converter()                             );
+        addFunction( new Func_pomoStateConverter()                             );
         addFunction( new Func_pomoRootFrequencies()                             );
         addFunction( new Func_pruneTree()                                       );
         addFunction( new Func_readPoMoCountFile()                               );
