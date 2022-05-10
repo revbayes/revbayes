1bd2bd436a5c5b52142e264a6103db6e349e2c36
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-05-10 13:40:28 +0200

    cleanup

--- src/core/io/CountFileToNaturalNumbersConverter.h
+++ src/core/io/CountFileToNaturalNumbersConverter.h
@@ -22,18 +22,17 @@ namespace RevBayesCore {
     public:
         CountFileToNaturalNumbersConverter();
 
-        void               cfconverter( const std::string &fi, const size_t n_individuals, const std::string &fo );
+        void                    cfconverter( const std::string &fi, const size_t n_individuals, const std::string &fo );
 
-        //const size_t     getNumberOfAlleles( void );
-        //const size_t     getNumberOfIndividuals( void );
+        //size_t                  getNumberOfAlleles( void );
+        //size_t                  getNumberOfIndividuals( void );
 
-        const size_t          getState(std::string counts, size_t n_alleles, size_t n_individuals, std::vector<std::string>& vector_edges, std::vector<int>& matrix_edges );
-        const size_t          sample_weight(size_t M, size_t m, size_t N);
-        const size_t          sample_edge(size_t allele_index, std::vector<int>& vector);
-        const size_t          get_index(std::vector<std::string>& vector, std::string element);
+        size_t                  getState(const std::string& counts, size_t n_alleles, size_t n_individuals, const std::vector<std::string>& vector_edges, const std::vector<int>& matrix_edges );
+        size_t                  sampleWeight(size_t M, size_t m, size_t N);
+        size_t                  sampleEdge(size_t allele_index, const std::vector<int>& vector);
+        size_t                  getIndex(const std::vector<std::string>& vector, const std::string& element);
 
     protected:
-
 
 
         
