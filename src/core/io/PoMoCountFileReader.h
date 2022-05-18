7a55100018cab2e11305c681b43e42e60bbe2713
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-05-18 07:58:58 +0200

    revising reading of PoMo count files

--- src/core/io/PoMoCountFileReader.h
+++ src/core/io/PoMoCountFileReader.h
@@ -12,42 +12,48 @@
 namespace RevBayesCore {
 
 
-	/**
-	 * Reader for allele count filesfor use with PoMo matrices.
-	 * Same format as read by IQ-Tree.
-	 * Description: http://www.iqtree.org/doc/Polymorphism-Aware-Models/
-	 * The first line contains COUNTSFILE  then the number of populations and the number of sites.
-	 * The following line contains the header: chromosome, site position, population names.
-	 * The following lines contain the states.
-	 *
-	 *
-	 *
-	 * @copyright Copyright 2009-
-	 * @author The RevBayes Development Core Team (Bastien Boussau)
-	 * @since 2015-03-03, version 1.0
-	 *
-	 */
-	class PoMoCountFileReader : public DelimitedDataReader {
+    /**
+     * Reader for allele count filesfor use with PoMo matrices.
+     * Same format as read by IQ-Tree.
+     * Description: http://www.iqtree.org/doc/Polymorphism-Aware-Models/
+     * The first line contains COUNTSFILE  then the number of populations and the number of sites.
+     * The following line contains the header: chromosome, site position, population names.
+     * The following lines contain the states.
+     *
+     *
+     *
+     * @copyright Copyright 2009-
+     * @author The RevBayes Development Core Team (Bastien Boussau)
+     * @since 2015-03-03, version 1.0
+     *
+     */
+    class PoMoCountFileReader : public DelimitedDataReader {
 
-	public:
+    public:
+        
+        enum FORMAT { PoMo, NaturalNumbers };
 
-		PoMoCountFileReader(const std::string &fn, const size_t virtualPopulationSize = 9, std::string d="", size_t ns=0);
+        PoMoCountFileReader(const std::string& fn, const size_t vps = 10, FORMAT f=FORMAT::PoMo);
+        PoMoCountFileReader(const PoMoCountFileReader& r);
+        virtual                                                 ~PoMoCountFileReader();
+        
+        PoMoCountFileReader&                                    operator=(const PoMoCountFileReader& n);                                         //!< Assignment operator
 
-		const size_t 																getNumberOfPopulations( void );
-		const size_t 																getNumberOfSites( void );
-		HomologousDiscreteCharacterData<PoMoState>*                                 getMatrix( void );
-		const size_t 																getVirtualPopulationSize( void );
+        
+        const size_t                                            getNumberOfPopulations( void );
+        const size_t                                            getNumberOfSites( void );
+        const AbstractHomologousDiscreteCharacterData&          getMatrix( void );
+        const size_t                                            getVirtualPopulationSize( void );
 
-	protected:
+    protected:
 
-		//std::vector<std::string>                        names;
-		//MatrixReal          							matrix;
-		size_t numberOfPopulations_;
-		size_t numberOfSites_;
-		size_t virtualPopulationSize_;
-		std::vector<string> names_;
-		HomologousDiscreteCharacterData<PoMoState>* matrix_;
-	};
+        size_t                                                  number_of_populations;
+        size_t                                                  number_of_sites;
+        size_t                                                  virtual_population_size;
+        std::vector<string>                                     names;
+        AbstractHomologousDiscreteCharacterData*                matrix;
+        FORMAT                                                  data_format;
+    };
 
 }
 
