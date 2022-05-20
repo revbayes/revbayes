c758b727d356247a0e7203112e0f1e714c79ca0d
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-05-20 10:36:26 +0200

    Adding a new feature to translate any character into it's index of the state

--- src/core/datatypes/phylogenetics/character/CharacterTranslator.h
+++ src/core/datatypes/phylogenetics/character/CharacterTranslator.h
@@ -7,40 +7,44 @@
 #include "DiscreteTaxonData.h"
 
 namespace RevBayesCore {
+class AbstractHomologousDiscreteCharacterData;
 class AminoAcidState;
 class CodonState;
 class DnaState;
 class RnaState;
 class DoubletState;
+class NaturalNumbersState;
     
     namespace CharacterTranslator {
     
+        AbstractHomologousDiscreteCharacterData*    constructDataMatrix( const std::string& type );
     
-        AbstractDiscreteTaxonData*              translateCharacters(const AbstractDiscreteTaxonData &d, const std::string& type);             
+        AbstractDiscreteTaxonData*                  translateCharacters(const AbstractDiscreteTaxonData &d, const std::string& type);
     
-        DiscreteTaxonData<CodonState>*          translateToCodon(const AbstractDiscreteTaxonData &d);
-        DiscreteTaxonData<CodonState>*          translateToCodonFromDna(const DiscreteTaxonData<DnaState> &d);
-        DiscreteTaxonData<CodonState>*          translateToCodonFromRna(const DiscreteTaxonData<RnaState> &d);
+        DiscreteTaxonData<CodonState>*              translateToCodon(const AbstractDiscreteTaxonData &d);
+        DiscreteTaxonData<CodonState>*              translateToCodonFromDna(const DiscreteTaxonData<DnaState> &d);
+        DiscreteTaxonData<CodonState>*              translateToCodonFromRna(const DiscreteTaxonData<RnaState> &d);
 
-        DiscreteTaxonData<CodonState>*          translateToDoublet(const AbstractDiscreteTaxonData &d);
-        DiscreteTaxonData<DoubletState>*        translateToDoubletFromDna(const DiscreteTaxonData<DnaState> &d);
-        DiscreteTaxonData<DoubletState>*        translateToDoubletFromRna(const DiscreteTaxonData<RnaState> &d);
+        DiscreteTaxonData<CodonState>*              translateToDoublet(const AbstractDiscreteTaxonData &d);
+        DiscreteTaxonData<DoubletState>*            translateToDoubletFromDna(const DiscreteTaxonData<DnaState> &d);
+        DiscreteTaxonData<DoubletState>*            translateToDoubletFromRna(const DiscreteTaxonData<RnaState> &d);
 
-        DiscreteTaxonData<DnaState>*            translateToDna(const AbstractDiscreteTaxonData &d);
+        DiscreteTaxonData<DnaState>*                translateToDna(const AbstractDiscreteTaxonData &d);
 //        DiscreteTaxonData<DnaState>*            translateToDna(const DiscreteTaxonData<AminoAcidState> &d);                                   
 //        DiscreteTaxonData<DnaState>*            translateToDna(const DiscreteTaxonData<CodonState> &d);                                       
 //        DiscreteTaxonData<DnaState>*            translateToDna(const DiscreteTaxonData<DnaState> &d);                                         
 //        DiscreteTaxonData<DnaState>*            translateToDna(const DiscreteTaxonData<RnaState> &d);                                         
         
-        DiscreteTaxonData<RnaState>*            translateToRna(const AbstractDiscreteTaxonData &d);                                           
+        DiscreteTaxonData<RnaState>*                translateToRna(const AbstractDiscreteTaxonData &d);
 //        DiscreteTaxonData<RnaState>*            translateToRna(const DiscreteTaxonData<AminoAcidState> &d);                                   
 
         
-        DiscreteTaxonData<AminoAcidState>*      translateToAa(const AbstractDiscreteTaxonData &d);                                            
+        DiscreteTaxonData<AminoAcidState>*          translateToAa(const AbstractDiscreteTaxonData &d);
 //        DiscreteTaxonData<AminoAcidState>*      translateToAa(const DiscreteTaxonData<AminoAcidState> &d);                                    
-        DiscreteTaxonData<AminoAcidState>*      translateToAaFromDna(const DiscreteTaxonData<DnaState> &d);
+        DiscreteTaxonData<AminoAcidState>*          translateToAaFromDna(const DiscreteTaxonData<DnaState> &d);
 
-        
+        DiscreteTaxonData<NaturalNumbersState>*     translateToNaturalNumbers(const AbstractDiscreteTaxonData &d);
+
         
     }
     
