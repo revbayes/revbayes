f37b433ac71a8cf5a42256c64eec901bc6906697
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-05-25 08:58:29 +0200

    Adding the StairwayPlot method

---
+++ src/core/distributions/popgen/StairwayPlotDistribution.cpp
@@ -0,0 +1,212 @@
+#include "StairwayPlotDistribution.h"
+
+#include <stddef.h>
+
+#include "RandomNumberFactory.h"
+#include "RbMathFunctions.h"
+#include "Cloneable.h"
+#include "Simplex.h"
+#include "TypedDagNode.h"
+
+namespace RevBayesCore { class DagNode; }
+
+using namespace RevBayesCore;
+
+/*StairwayPlot Distribution Constructor
+ * @param p A simplex of the the probabilities for each category
+ * @param n A long for the number of trials
+ */
+
+StairwayPlotDistribution::StairwayPlotDistribution(const TypedDagNode< RbVector<double> > *th, long n, long n_ind, bool f) : TypedDistribution< RbVector<long> >( new RbVector<long>() ),
+    theta( th ),
+    num_sites( n ),
+    folded( f ),
+    num_individuals( n_ind )
+{
+    // add the parameters to our set (in the base class)
+    // in that way other class can easily access the set of our parameters
+    // this will also ensure that the parameters are not getting deleted before we do
+    addParameter( theta );
+    
+    initialize();
+}
+
+
+StairwayPlotDistribution::~StairwayPlotDistribution( void )
+{
+    // We don't delete the parameters, because they might be used somewhere else too.
+    // The model needs to do that!
+}
+
+
+void StairwayPlotDistribution::calculateExpectedSFS(void) const
+{
+    // get the thetas for easier handling
+    const RbVector<double>& th = theta->getValue();
+
+    // variable for the sum of all non-monomorphic frequencies
+    double sum_expected_frequency = 0.0;
+        
+    for (size_t i=1; i<num_individuals; ++i)
+    {
+        double expected_frequency = 0.0;
+        for (size_t k=2; k<=(num_individuals-i+1); ++k)
+        {
+            double this_theta = th[k-2];
+            expected_frequency += this_theta * prob_k[i-1][k-2];
+        }
+        
+        // add the current expected frequency to our sum
+        sum_expected_frequency += expected_frequency;
+        
+        // store the value of the expected frequency
+        expected_SFS[i] = expected_frequency;
+        
+        // maybe store the log value too???
+    }
+    
+    // now normalize
+    for (size_t i=1; i<num_individuals; ++i)
+    {
+        expected_SFS[i] /= sum_expected_frequency;
+    }
+    
+//    // now also store the probability for the monorphic sites
+//    expected_SFS[0] = 1.0 - sum_expected_frequency;
+}
+
+
+
+StairwayPlotDistribution* StairwayPlotDistribution::clone( void ) const
+{
+    return new StairwayPlotDistribution( *this );
+}
+
+
+double StairwayPlotDistribution::computeLnProbability( void )
+{
+    
+    // initialize the probability
+    double ln_prob = 0;
+    
+    // get the data, i.e., the observed counts for the frequencies
+    const RbVector<long>& obs_sfs_counts = *value;
+
+    // compute the expected SFS, i.e., the expected frequency of observing a site with frequency i
+    calculateExpectedSFS();
+    
+    size_t max_freq = num_individuals;
+    if ( folded == true )
+    {
+        max_freq = ceil( (num_individuals+1) / 2.0);
+    }
+    
+    // Sebastian: Note, we cannot compute the frequency for monomorphic sites.
+    for (size_t i=1; i<max_freq; ++i)
+    {
+        
+        // compute the multinomial probability for the SFS frequency
+        if ( folded == false )
+        {
+            ln_prob -= RbMath::lnGamma((double)obs_sfs_counts[i] + 1.0);
+            ln_prob += (double)obs_sfs_counts[i] * log(expected_SFS[i]);
+        }
+        else
+        {
+            ln_prob -= RbMath::lnGamma((double)obs_sfs_counts[i] + 1.0);
+            if ( i == (num_individuals/2.0) )
+            {
+                ln_prob += (double)obs_sfs_counts[i] * log(expected_SFS[i]);
+            }
+            else
+            {
+                ln_prob += (double)obs_sfs_counts[i] * log(expected_SFS[i]+expected_SFS[num_individuals-i+1]);
+            }
+        }
+    }
+    
+    // divide by the factorial of the total number of observation
+//    ln_prob -= RbMath::lnGamma((double)num_sites->getValue() + 1.0);
+    ln_prob -= ln_factorial_num_sites;
+
+    
+    return ln_prob;
+}
+
+RbVector<double> StairwayPlotDistribution::computeTimeBreakpoints( void ) const
+{
+    const RbVector<double>& th = theta->getValue();
+    RbVector<double> times = RbVector<double>(num_individuals-1,0);
+    for (size_t i=2; i<=num_individuals; ++i)
+    {
+        double this_time = 0.0;
+        for (size_t k=i; k<=num_individuals; ++k)
+        {
+            this_time += th[k-2]/(i*(i-1));
+        }
+        times[i-2] = this_time;
+    }
+    
+    return times;
+}
+
+
+
+void StairwayPlotDistribution::executeMethod(const std::string &name, const std::vector<const DagNode *> &args, RbVector<double> &rv) const
+{
+   
+    if ( name == "getTimes" )
+    {
+        rv = computeTimeBreakpoints();
+    }
+    else
+    {
+        throw RbException("The state dependent birth-death process does not have a member method called '" + name + "'.");
+    }
+
+}
+
+
+void StairwayPlotDistribution::initialize( void )
+{
+    
+    // allocate/resize the expected SFS frequency vector
+    expected_SFS.clear();
+    expected_SFS.resize( num_individuals+1 );
+    
+    prob_k.clear();
+    prob_k.resize( num_individuals-1 );
+    
+    for (size_t i=1; i<num_individuals; ++i)
+    {
+        std::vector<double>& this_freq_prob_k = prob_k[i-1];
+        this_freq_prob_k.resize(num_individuals-i+1);
+        for (size_t k=2; k<=(num_individuals-i+1); ++k)
+        {
+            this_freq_prob_k[k-2] = exp( RbMath::lnGamma(num_individuals-i) +
+                                         RbMath::lnGamma(num_individuals-k+1) -
+                                         RbMath::lnGamma(num_individuals) -
+                                         RbMath::lnGamma(num_individuals-i-k+2) );
+        }
+    }
+    
+    ln_factorial_num_sites = RbMath::lnGamma( num_sites );
+}
+
+
+void StairwayPlotDistribution::redrawValue( void )
+{
+    *value = RbVector<long>( num_individuals+1, 1 );
+}
+
+
+/** Swap a parameter of the distribution */
+void StairwayPlotDistribution::swapParameterInternal(const DagNode *oldP, const DagNode *newP)
+{
+    
+    if (oldP == theta)
+    {
+        theta = static_cast<const TypedDagNode< RbVector<double> >* >( newP );
+    }
+    
+}
---
+++ src/revlanguage/distributions/popgen/Dist_StairwayPlot.cpp
@@ -0,0 +1,190 @@
+#include <math.h>
+#include <stddef.h>
+#include <ostream>
+#include <string>
+#include <vector>
+
+#include "ArgumentRule.h"
+#include "ArgumentRules.h"
+#include "Dist_StairwayPlot.h"
+#include "StairwayPlotDistribution.h"
+#include "RbVector.h"
+#include "RlSimplex.h"
+#include "ConstantNode.h"
+#include "DagMemberFunction.h"
+#include "DagNode.h"
+#include "DeterministicNode.h"
+#include "DistributionMemberFunction.h"
+#include "DynamicNode.h"
+#include "IndirectReferenceFunction.h"
+#include "ModelObject.h"
+#include "ModelVector.h"
+#include "Natural.h"
+#include "RbHelpReference.h"
+#include "RevObject.h"
+#include "RevPtr.h"
+#include "RevVariable.h"
+#include "RlConstantNode.h"
+#include "RlDagMemberFunction.h"
+#include "RlDeterministicNode.h"
+#include "RlDistributionMemberFunction.h"
+#include "RlStochasticNode.h"
+#include "RlTypedDistribution.h"
+#include "RlTypedFunction.h"
+#include "StochasticNode.h"
+#include "TypeSpec.h"
+#include "TypedDagNode.h"
+#include "TypedDistribution.h"
+#include "TypedFunction.h"
+#include "UserFunctionNode.h"
+
+namespace RevBayesCore { class Simplex; }
+
+using namespace RevLanguage;
+
+Dist_StairwayPlot::Dist_StairwayPlot(void) : TypedDistribution< ModelVector<Natural> >()
+{
+    
+}
+
+
+Dist_StairwayPlot::~Dist_StairwayPlot(void)
+{
+    
+}
+
+
+
+Dist_StairwayPlot* Dist_StairwayPlot::clone( void ) const
+{
+    
+    return new Dist_StairwayPlot(*this);
+}
+
+
+RevBayesCore::StairwayPlotDistribution* Dist_StairwayPlot::createDistribution( void ) const
+{
+    
+    // get the parameters
+    long                                                            n_sites = static_cast<const Natural              &>( num_sites->getRevObject() ).getValue();
+    long                                                            n_ind   = static_cast<const Natural              &>( num_individuals->getRevObject() ).getValue();
+    long                                                            f       = static_cast<const RlBoolean            &>( folded->getRevObject() ).getValue();
+    RevBayesCore::TypedDagNode< RevBayesCore::RbVector<double> >*   th      = static_cast<const ModelVector<RealPos> &>( theta->getRevObject() ).getDagNode();
+    RevBayesCore::StairwayPlotDistribution*                         d       = new RevBayesCore::StairwayPlotDistribution( th, n_sites, n_ind, f );
+    return d;
+}
+
+
+
+/* Get Rev type of object */
+const std::string& Dist_StairwayPlot::getClassType(void)
+{
+    
+    static std::string rev_type = "Dist_StairwayPlot";
+    return rev_type;
+}
+
+/* Get class type spec describing type of object */
+const TypeSpec& Dist_StairwayPlot::getClassTypeSpec(void)
+{
+    
+    static TypeSpec rev_type_spec = TypeSpec( getClassType(), new TypeSpec( TypedDistribution< ModelVector<Natural> >::getClassTypeSpec() ) );
+    return rev_type_spec;
+}
+
+
+/**
+ * Get the Rev name for the distribution.
+ * This name is used for the constructor and the distribution functions,
+ * such as the density and random value function
+ *
+ * \return Rev name of constructor function.
+ */
+std::string Dist_StairwayPlot::getDistributionFunctionName( void ) const
+{
+    // create a distribution name variable that is the same for all instance of this class
+    std::string d_name = "StairwayPlot";
+    
+    return d_name;
+}
+
+
+MethodTable Dist_StairwayPlot::getDistributionMethods( void ) const
+{
+    MethodTable methods = TypedDistribution< ModelVector<Natural> >::getDistributionMethods();
+    
+    ArgumentRules* times_arg_rules = new ArgumentRules();
+    methods.addFunction( new DistributionMemberFunction<Dist_StairwayPlot, ModelVector<RealPos> >( "getTimes", variable, times_arg_rules, true, true ) );
+
+    return methods;
+}
+
+
+
+/** Return member rules (no members) */
+const MemberRules& Dist_StairwayPlot::getParameterRules(void) const
+{
+    
+    static MemberRules dist_member_rules;
+    static bool rules_set = false;
+    
+    if ( rules_set == false )
+    {
+        dist_member_rules.push_back( new ArgumentRule( "theta", ModelObject<RealPos>::getClassTypeSpec(), "The theta values with theta=4*Ne*mu. We expect n-1 theta values where n is the number of individuals.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        dist_member_rules.push_back( new ArgumentRule( "numSites", Natural::getClassTypeSpec(), "The number of sites in the SFS.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        dist_member_rules.push_back( new ArgumentRule( "numIndividuals", Natural::getClassTypeSpec(), "The number of individuals in (unfolded) the SFS.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        dist_member_rules.push_back( new ArgumentRule( "folded", Natural::getClassTypeSpec(), "Is the site frequency folded.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY, new RlBoolean(false) ) );
+
+        rules_set = true;
+    }
+    
+    return dist_member_rules;
+}
+
+
+const TypeSpec& Dist_StairwayPlot::getTypeSpec( void ) const
+{
+    
+    static TypeSpec ts = getClassTypeSpec();
+    return ts;
+}
+
+
+/** Print value for user */
+void Dist_StairwayPlot::printValue(std::ostream& o) const
+{
+    
+    o << "StairwayPlot(theta=";
+    if ( theta != NULL )
+        o << theta->getName();
+    else
+        o << "?";
+    o << ")";
+}
+
+
+/** Set a member variable */
+void Dist_StairwayPlot::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
+{
+    
+    if ( name == "theta" )
+    {
+        theta = var;
+    }
+    else if ( name == "numSites" )
+    {
+        num_sites = var;
+    }
+    else if ( name == "numIndividuals" )
+    {
+        num_individuals = var;
+    }
+    else if ( name == "folded" )
+    {
+        folded = var;
+    }
+    else
+    {
+        TypedDistribution< ModelVector<Natural> >::setConstParameter(name, var);
+    }
+}
