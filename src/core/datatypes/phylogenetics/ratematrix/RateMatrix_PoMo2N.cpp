c95f5ea12d43cae235e3c826714346cfbe04822e
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-06-09 08:58:40 +0200

    Playing around with a correction for the virtual population size

--- src/core/datatypes/phylogenetics/ratematrix/RateMatrix_PoMo2N.cpp
+++ src/core/datatypes/phylogenetics/ratematrix/RateMatrix_PoMo2N.cpp
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <cstddef>
 
+#include <boost/math/special_functions/digamma.hpp>
+
 #include "RbException.h"
 #include "Assignable.h"
 #include "Cloneable.h"
@@ -27,15 +29,20 @@ phi( in_k, 1.0 )
 */
 
 /** Construct rate matrix with n states, an exchangeability matrix, a simplex of equilibrium frequencies, and a virtual population size */
-RateMatrix_PoMo2N::RateMatrix_PoMo2N(long num_states, long in_n )  : 
+RateMatrix_PoMo2N::RateMatrix_PoMo2N(long num_states, long in_n, bool mu_corr, bool d_corr )  :
 AbstractRateMatrix( num_states ), 
 N( in_n ),
 mu( 2 , 0.01 ),
-phi( 2 , 1.0 )
+phi( 2 , 1.0 ),
+use_mutation_correction( mu_corr ),
+use_drift_correction( d_corr ),
+N_eff( 1000.0 )
 {
     theEigenSystem       = new EigenSystem(the_rate_matrix);
     c_ijk.resize(num_states * num_states * num_states);
     cc_ijk.resize(num_states * num_states * num_states);
+    
+    harmonic_number_M = boost::math::digamma(N) - boost::math::digamma(1.0);
   
     update();
 }
@@ -45,7 +52,12 @@ RateMatrix_PoMo2N::RateMatrix_PoMo2N(const RateMatrix_PoMo2N& m) :
 AbstractRateMatrix( m ), 
 N( m.N ),
 mu( m.mu ),
-phi( m.phi )
+phi( m.phi ),
+use_mutation_correction( m.use_mutation_correction ),
+use_drift_correction( m.use_drift_correction ),
+N_eff( m.N_eff),
+harmonic_number_M( m.harmonic_number_M ),
+harmonic_number_N( m.harmonic_number_N )
 {
 
     
@@ -75,7 +87,14 @@ RateMatrix_PoMo2N& RateMatrix_PoMo2N::operator=(const RateMatrix_PoMo2N &r)
         N                   = r.N;
         mu                  = r.mu;
         phi                 = r.phi;
-                
+        
+        use_mutation_correction = r.use_mutation_correction;
+        use_drift_correction    = r.use_drift_correction;
+        
+        N_eff               = r.N_eff;
+        harmonic_number_M   = r.harmonic_number_M;
+        harmonic_number_N   = r.harmonic_number_N;
+        
         
         delete theEigenSystem;
         
@@ -109,6 +128,8 @@ RateMatrix_PoMo2N& RateMatrix_PoMo2N::assign(const Assignable &m)
 
 double RateMatrix_PoMo2N::averageRate(void) const
 {
+    throw RbException("Missing implementation of average rate in PoMo2N.");
+    
     return 1.0;
 }
 
@@ -119,101 +140,114 @@ double RateMatrix_PoMo2N::averageRate(void) const
 void RateMatrix_PoMo2N::buildRateMatrix(void)
 {
 
- /*  
-  INFORMATION ABOUT THE PoMo2N RATE MATRIX
+    /*
+     INFORMATION ABOUT THE PoMo2N RATE MATRIX
 
-  It includes both fixed and polymorphic sites, but only biallelic states are considered.
+     It includes both fixed and polymorphic sites, but only biallelic states are considered.
 
-  The pomo rate matrices defined here first list the fixed states {Na0}, {Na1} ...,
-  these occupying positions 0:(K-1), and then polymorphic states.
+     The pomo rate matrices defined here first list the fixed states {Na0}, {Na1} ...,
+     these occupying positions 0:(K-1), and then polymorphic states.
 
-  K alleles comprise (K*K-K)/2 pairwise combinations of alleles.
-  This is the number of edges in the pomo state-space. Each edge comprises N-1 polymorphic states, 
-  each of which represents a state of allelic frequencies in the population (summing to N).
-  Example for a random allele pair aiaj: {(N-1)ai,1aj}, {(N-2)ai,2aj}, ..., {1ai,(N-1)aj}
+     K alleles comprise (K*K-K)/2 pairwise combinations of alleles.
+     This is the number of edges in the pomo state-space. Each edge comprises N-1 polymorphic states,
+     each of which represents a state of allelic frequencies in the population (summing to N).
+     Example for a random allele pair aiaj: {(N-1)ai,1aj}, {(N-2)ai,2aj}, ..., {1ai,(N-1)aj}
 
-  The polymorphic edges are listed in the following order a0a1, a0a2, a0a3, ..., a(K-2)aK-1
-  We say the a0a1 polymorphic states sit at edge 0. Thus, {(N-1)a0,1a1} sits at position K and 
-  {1a0,(N-1)a1} sits N-2 positions further (i.e., K+N-2).
-  More generally, the polymorphic states of the allele pair sitting at edge E occupy the positions
-  [K+E*N-E]:[K+(E+1)*(N-1)-1].
-  */
+     The polymorphic edges are listed in the following order a0a1, a0a2, a0a3, ..., a(K-2)aK-1
+     We say the a0a1 polymorphic states sit at edge 0. Thus, {(N-1)a0,1a1} sits at position K and
+     {1a0,(N-1)a1} sits N-2 positions further (i.e., K+N-2).
+     More generally, the polymorphic states of the allele pair sitting at edge E occupy the positions
+     [K+E*N-E]:[K+(E+1)*(N-1)-1].
+     */
 
-   MatrixReal& m = *the_rate_matrix;
+    MatrixReal& m = *the_rate_matrix;
 
-  //populate rate matrix with 0.0
-  // **total waste of time with sparse matrices like pomos**
-  for (int i=0; i< num_states; i++){
-    for (int j=0; j< num_states; j++){
-      m[i][j] = 0.0;        
-    }
-  }
-
-
-  // set the diagonal vector
-  std::vector< double > diagonal(num_states,0.0); 
-
-
-  //mutations
-  //mutations
-  m[0][2]  = N*mu[0];  //{Na0} -> {(N-1)a0,1a1}
-  m[1][N]  = N*mu[1];  //{Na1} -> {1a0,(N-1)a1}
-  diagonal[0] = m[0][2];
-  diagonal[1] = m[1][N];
-
-  //fixations
-  //fixations
-  m[2][0]  = (N-1.0)*phi[0]/((N-1)*phi[0]+phi[1]);  //{(N-1)a0,1a1} -> {Na0} 
-  m[N][1]  = (N-1.0)*phi[1]/((N-1)*phi[1]+phi[0]);  //{1a0,(N-1)a1} -> {Na1} 
-  diagonal[2] += m[2][0];
-  diagonal[N] += m[N][1];
-
-  //the pomo rate matrix is entirely defined by fixations and mutations if N=2
-  if (N>2) {
-
-    //frequency shifts from singletons
-    m[2][3]   = (N-1.0)*phi[1]/((N-1)*phi[0]+phi[1]);  //{(N-1)a0,1a1} -> {(N-2)a0,2a1}
-    m[N][N-1] = (N-1.0)*phi[0]/((N-1)*phi[1]+phi[0]);  //{1a0,(N-1)a1} -> {2a0,(N-2)a1}
-    diagonal[2] += m[2][3] ;
-    diagonal[N] += m[N][N-1];
-
-     //frequency shifts for all the other polymorphic states
-    if (N>3) {
-
-      //polymorphic states are populated
-      for (int n=2; n<(N-1); n++){
-
-        //ai aj
-        m[n+1][2+n]  = n*(N-n)*phi[1]/(n*phi[1]+(N-n)*phi[0]); //{naj,(N-n)ai} -> {(n+1)aj,(N-n-1)ai}
-        m[n+1][n]    = n*(N-n)*phi[0]/(n*phi[1]+(N-n)*phi[0]); //{naj,(N-n)ai} -> {(n-1)aj,(N-n+1)ai}
-        diagonal[n+1] += m[n+1][2+n] + m[n+1][n];
-
-      }
-
+    // populate rate matrix with 0.0
+    // **total waste of time with sparse matrices like pomos**
+    for (int i=0; i< num_states; i++)
+    {
+        for (int j=0; j< num_states; j++)
+        {
+            m[i][j] = 0.0;
+        }
     }
 
-  }
+
+    // set the diagonal vector
+    std::vector< double > diagonal(num_states,0.0);
+    
+    harmonic_number_N = boost::math::digamma(N_eff) - boost::math::digamma(1.0);
+    
+    double rescaling_factor_to_effective_population = (N*harmonic_number_M) / (N_eff*harmonic_number_N);
+    if ( use_drift_correction == false )
+    {
+        rescaling_factor_to_effective_population = 1.0;
+    }
+    
+    // mutations
+    if ( use_mutation_correction == false )
+    {
+        m[0][2]  = N*mu[0];  //{Na0} -> {(N-1)a0,1a1}
+        m[1][N]  = N*mu[1];  //{Na1} -> {1a0,(N-1)a1}
+    }
+    else
+    {
+        m[0][2]  = N_eff*mu[0];  //{Na0} -> {(N-1)a0,1a1}
+        m[1][N]  = N_eff*mu[1];  //{Na1} -> {1a0,(N-1)a1}
+    }
+    diagonal[0] = m[0][2];
+    diagonal[1] = m[1][N];
+
+    // fixations
+//    m[2][0]  = (N-1.0)*phi[0]/((N-1)*phi[0]+phi[1]);  //{(N-1)a0,1a1} -> {Na0}
+//    m[N][1]  = (N-1.0)*phi[1]/((N-1)*phi[1]+phi[0]);  //{1a0,(N-1)a1} -> {Na1}
+    m[2][0]  = ((N-1.0)*phi[0]/((N-1)*phi[0]+phi[1])) * rescaling_factor_to_effective_population;  //{(N-1)a0,1a1} -> {Na0}
+    m[N][1]  = ((N-1.0)*phi[1]/((N-1)*phi[1]+phi[0])) * rescaling_factor_to_effective_population;  //{1a0,(N-1)a1} -> {Na1}
+    diagonal[2] += m[2][0];
+    diagonal[N] += m[N][1];
+    
+
+    //the pomo rate matrix is entirely defined by fixations and mutations if N=2
+    if (N>2)
+    {
+
+        //frequency shifts from singletons
+//        m[2][3]   = (N-1.0)*phi[1]/((N-1)*phi[0]+phi[1]);  //{(N-1)a0,1a1} -> {(N-2)a0,2a1}
+//        m[N][N-1] = (N-1.0)*phi[0]/((N-1)*phi[1]+phi[0]);  //{1a0,(N-1)a1} -> {2a0,(N-2)a1}
+        m[2][3]   = ((N-1.0)*phi[1]/((N-1)*phi[0]+phi[1])) * rescaling_factor_to_effective_population;  //{(N-1)a0,1a1} -> {(N-2)a0,2a1}
+        m[N][N-1] = ((N-1.0)*phi[0]/((N-1)*phi[1]+phi[0])) * rescaling_factor_to_effective_population;  //{1a0,(N-1)a1} -> {2a0,(N-2)a1}
+        diagonal[2] += m[2][3] ;
+        diagonal[N] += m[N][N-1];
+
+        //frequency shifts for all the other polymorphic states
+        if (N>3)
+        {
+
+            //polymorphic states are populated
+            for (int n=2; n<(N-1); n++)
+            {
+
+                //ai aj
+//                m[n+1][2+n]  = n*(N-n)*phi[1]/(n*phi[1]+(N-n)*phi[0]); //{naj,(N-n)ai} -> {(n+1)aj,(N-n-1)ai}
+//                m[n+1][n]    = n*(N-n)*phi[0]/(n*phi[1]+(N-n)*phi[0]); //{naj,(N-n)ai} -> {(n-1)aj,(N-n+1)ai}
+                m[n+1][2+n]  = (n*(N-n)*phi[1]/(n*phi[1]+(N-n)*phi[0])) * rescaling_factor_to_effective_population; //{naj,(N-n)ai} -> {(n+1)aj,(N-n-1)ai}
+                m[n+1][n]    = (n*(N-n)*phi[0]/(n*phi[1]+(N-n)*phi[0])) * rescaling_factor_to_effective_population; //{naj,(N-n)ai} -> {(n-1)aj,(N-n+1)ai}
+                diagonal[n+1] += m[n+1][2+n] + m[n+1][n];
+
+            }
+
+        }
+
+    }
 
 
-  // set the diagonal values and calculate the reciprocal of the average rate
-  for (int j=0; j< num_states; j++){
-    m[j][j] = -diagonal[j];
-  }
+    // set the diagonal values and calculate the reciprocal of the average rate
+    for (int j=0; j< num_states; j++)
+    {
+        m[j][j] = -diagonal[j];
+    }
   
     
-  /*
-  //I cannot get the stationary frequencies by any means. So, I am just skiping the matrix normalization.
-  std::vector< double > stationaryFrequencies = getStationaryFrequencies();
-  double rRate = 0.0;
-  for (int j=0; j< num_states; j++){
-    rRate += diagonal[j]*stationaryVector[j];   
-    std::cout << "rate:" << stationaryVector[j] << "\n";
-  }
-
-  rRate = 1.0/rRate;
-  */
-
-
 }
 
 
@@ -295,9 +329,9 @@ std::vector<double> RateMatrix_PoMo2N::getStationaryFrequencies( void ) const
 }
 
 
-void RateMatrix_PoMo2N::setN( long & ni )
+void RateMatrix_PoMo2N::setNeff( long ni )
 {
-    N = ni;
+    N_eff = ni;
     
     // set flags
     needs_update = true;
