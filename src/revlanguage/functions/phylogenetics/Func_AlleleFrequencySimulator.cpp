29f17255a99803ecba96dc1f3ffe0e70c000c922
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-07-07 08:52:13 +0200

    Moran generations

--- src/revlanguage/functions/phylogenetics/Func_AlleleFrequencySimulator.cpp
+++ src/revlanguage/functions/phylogenetics/Func_AlleleFrequencySimulator.cpp
@@ -54,15 +54,16 @@ RevPtr<RevVariable> Func_AlleleFrequencySimulator::execute()
     
     std::vector<long> population_sizes      = static_cast<const ModelVector<Natural> &>( this->args[1].getVariable()->getRevObject() ).getValue();
     double generation_time                  = static_cast<const RealPos &>( this->args[2].getVariable()->getRevObject() ).getValue();
-    long num_sites                          = static_cast<const Natural &>( this->args[3].getVariable()->getRevObject() ).getValue();
-    std::vector<double> mutation_rates      = static_cast<const ModelVector<RealPos> &>( this->args[4].getVariable()->getRevObject() ).getValue();
-    std::vector<long> samples_per_specues   = static_cast<const ModelVector<Natural> &>( this->args[5].getVariable()->getRevObject() ).getValue();
-    double root_branch                      = static_cast<const RealPos &>( this->args[6].getVariable()->getRevObject() ).getValue();
-    bool variable                           = static_cast<const RlBoolean &>( this->args[7].getVariable()->getRevObject() ).getValue();
-    const std::string& fn                   = static_cast<const RlString &>( this->args[8].getVariable()->getRevObject() ).getValue();
+    bool moran_generations                  = static_cast<const RlBoolean &>( this->args[3].getVariable()->getRevObject() ).getValue();
+    long num_sites                          = static_cast<const Natural &>( this->args[4].getVariable()->getRevObject() ).getValue();
+    std::vector<double> mutation_rates      = static_cast<const ModelVector<RealPos> &>( this->args[5].getVariable()->getRevObject() ).getValue();
+    std::vector<long> samples_per_species   = static_cast<const ModelVector<Natural> &>( this->args[6].getVariable()->getRevObject() ).getValue();
+    double root_branch                      = static_cast<const RealPos &>( this->args[7].getVariable()->getRevObject() ).getValue();
+    bool variable                           = static_cast<const RlBoolean &>( this->args[8].getVariable()->getRevObject() ).getValue();
+    const std::string& fn                   = static_cast<const RlString &>( this->args[9].getVariable()->getRevObject() ).getValue();
 
 
-    RevBayesCore::AlleleFrequencySimulator sim = RevBayesCore::AlleleFrequencySimulator(tree, population_sizes, generation_time, num_sites, mutation_rates, samples_per_specues, root_branch );
+    RevBayesCore::AlleleFrequencySimulator sim = RevBayesCore::AlleleFrequencySimulator(tree, population_sizes, generation_time, num_sites, mutation_rates, samples_per_species, root_branch, moran_generations );
     sim.simulateAlleleFrequencies( fn, variable );
     
     return NULL;
@@ -82,6 +83,7 @@ const ArgumentRules& Func_AlleleFrequencySimulator::getArgumentRules( void ) con
         argument_rules.push_back( new ArgumentRule( "tree"              , TimeTree::getClassTypeSpec(), "The tree along which we want to simulate.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
         argument_rules.push_back( new ArgumentRule( "populationSizes"   , ModelVector<Natural>::getClassTypeSpec(), "The population sizes for all branches, including the root branch.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
         argument_rules.push_back( new ArgumentRule( "generationTime"    , RealPos::getClassTypeSpec(), "The generation time for the simulations.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
+        argument_rules.push_back( new ArgumentRule( "moranGenerations"  , RlBoolean::getClassTypeSpec(), "Is the generation time in Moran generation, i.e., scaled by N.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
         argument_rules.push_back( new ArgumentRule( "numSites"          , Natural::getClassTypeSpec(), "The number of sites to simulate.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
         argument_rules.push_back( new ArgumentRule( "mutationRates"     , ModelVector<RealPos>::getClassTypeSpec(), "The mutation rates from 0 to 1 and 1 to 0.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
         argument_rules.push_back( new ArgumentRule( "samplesPerSpecies" , ModelVector<Natural>::getClassTypeSpec(), "The observed number of samples per species.", ArgumentRule::BY_VALUE, ArgumentRule::ANY ) );
