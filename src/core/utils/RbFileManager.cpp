a8bfc9ee58071e9c0398abae58a331743cd9b232
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-07-09 17:52:19 -0400

    Declare safeGetline( ) and expandUserDir( ) inside namespace RevBayesCore.
    This fixes a problem with linking.

--- src/core/utils/RbFileManager.cpp
+++ src/core/utils/RbFileManager.cpp
@@ -118,6 +118,50 @@ void RbFileManager::createDirectoryForFile( void )
     fs::create_directories(dir_path);
 }
 
+namespace RevBayesCore
+{
+
+/** Get line while safely handling cross-platform line endings.
+ *  Modified from: https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
+ *
+ * @param[in] is stream from which to read
+ * @param[out] t string to store the line read
+ *
+ * @return the input stream
+ * */
+std::istream& safeGetline(std::istream& is, std::string& t)
+{
+    t.clear();
+
+    // The characters in the stream are read one-by-one using a std::streambuf.
+    // That is faster than reading them one-by-one using the std::istream.
+    // Code that uses streambuf this way must be guarded by a sentry object.
+    // The sentry object performs various tasks,
+    // such as thread synchronization and updating the stream state.
+
+    std::istream::sentry se(is, true);
+    std::streambuf* sb = is.rdbuf();
+
+    for(;;) {
+        int c = sb->sbumpc();
+        switch (c) {
+            case '\n':
+                return is;
+            case '\r':
+                if(sb->sgetc() == '\n')
+                    sb->sbumpc();
+                return is;
+            case EOF:
+                // Also handle the case when the last line has no line ending
+                if(t.empty())
+                    is.setstate(std::ios::eofbit);
+                return is;
+            default:
+                t += (char)c;
+        }
+    }
+}
+
 
 /**
  * Portable code to get a full path by expanding the user home directory.
@@ -153,7 +197,7 @@ std::string expandUserDir(std::string path)
     return path;
 }
 
-
+}
 /** Format an error exception string for problems specifying the file/path name
  * @param[out] errorStr string to store the formatted error
 */
@@ -689,48 +733,6 @@ bool RbFileManager::parsePathFileNames(const std::string &input_string)
     }
     
     return true;
-}
-
-
-/** Get line while safely handling cross-platform line endings.
- *  Modified from: https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf
- *
- * @param[in] is stream from which to read
- * @param[out] t string to store the line read
- *
- * @return the input stream
- * */
-std::istream& safeGetline(std::istream& is, std::string& t)
-{
-    t.clear();
-    
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
-    
-    std::istream::sentry se(is, true);
-    std::streambuf* sb = is.rdbuf();
-    
-    for(;;) {
-        int c = sb->sbumpc();
-        switch (c) {
-            case '\n':
-                return is;
-            case '\r':
-                if(sb->sgetc() == '\n')
-                    sb->sbumpc();
-                return is;
-            case EOF:
-                // Also handle the case when the last line has no line ending
-                if(t.empty())
-                    is.setstate(std::ios::eofbit);
-                return is;
-            default:
-                t += (char)c;
-        }
-    }
 }
 
 
