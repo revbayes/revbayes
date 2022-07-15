254b442fd189ef6b85cd71c586f126fbd0f22c05
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-07-15 15:14:08 +0200

    adding an exponential rng

--- src/core/math/RandomNumberGenerator.cpp
+++ src/core/math/RandomNumberGenerator.cpp
@@ -72,3 +72,64 @@ double RandomNumberGenerator::uniform01(void)
 	// Returns a pseudo-random number between 0 and 1.
     return last_u;
 }
+
+
+/*!
+ *
+ * \brief Random variates from the standard exponential distribution.
+ * \return Returns a exponentially-distributed random variable with rate 1.
+ * \throws Does not throw an error.
+ */
+double RandomNumberGenerator::exponential(void)
+{
+    /* q[k-1] = sum(log(2)^k / k!)  k=1,..,n, */
+    /* The highest n (here 16) is determined by q[n-1] = 1.0 */
+    /* within standard precision */
+    const static double q[] =
+    {
+        0.6931471805599453,
+        0.9333736875190459,
+        0.9888777961838675,
+        0.9984959252914960,
+        0.9998292811061389,
+        0.9999833164100727,
+        0.9999985691438767,
+        0.9999998906925558,
+        0.9999999924734159,
+        0.9999999995283275,
+        0.9999999999728814,
+        0.9999999999985598,
+        0.9999999999999289,
+        0.9999999999999968,
+        0.9999999999999999,
+        1.0000000000000000
+    };
+
+    double a = 0.;
+    double u = uniform01();    /* precaution if u = 0 is ever returned */
+    while(u <= 0. || u >= 1.) u = uniform01();
+    for (;;)
+    {
+        u += u;
+        if (u > 1.)
+            break;
+        a += q[0];
+    }
+    u -= 1.;
+
+    if (u <= q[0])
+        return a + u;
+
+    int i = 0;
+    double ustar = uniform01(), umin = ustar;
+    do {
+        ustar = uniform01();
+
+        if (umin > ustar)
+            umin = ustar;
+        
+        i++;
+    } while (u > q[i]);
+
+    return a + umin * q[0];
+}
