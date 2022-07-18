9b21d64d47028b8a0a61660c439c26a15eb3851b
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-07-18 10:00:31 +0200

    MPI

---
+++ src/revlanguage/datatypes/phylogenetics/RlAlleleFrequencySimulator.hpp
@@ -0,0 +1,13 @@
+//
+//  RlAlleleFrequencySimulator.hpp
+//  RevBayesDev
+//
+//  Created by Sebastian Hoehna on 7/18/22.
+//
+
+#ifndef RlAlleleFrequencySimulator_hpp
+#define RlAlleleFrequencySimulator_hpp
+
+#include <stdio.h>
+
+#endif /* RlAlleleFrequencySimulator_hpp */
