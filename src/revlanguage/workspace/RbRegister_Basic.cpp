e5541130420341611567e9b11312b0042bbdfe97
Sebastian Hoehna <sebastian.hoehna@gmail.com>
2022-07-31 21:19:03 +0200

    adding rev for vector of probs

--- src/revlanguage/workspace/RbRegister_Basic.cpp
+++ src/revlanguage/workspace/RbRegister_Basic.cpp
@@ -308,6 +308,7 @@ void RevLanguage::Workspace::initializeBasicGlobalWorkspace(void)
         addFunction( new Func_reverse<Real>()               );
         addFunction( new Func_reverse<Natural>()            );
         addFunction( new Func_reverse<RealPos>()            );
+        addFunction( new Func_reverse<Probability>()        );
         addFunction( new Func_reverse<RlString>()           );
         addFunction( new Func_reverse<RlBoolean>()          );
         addFunction( new Func_clear()                       );
