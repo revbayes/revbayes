93f40cf94c395837786d0788b1cceb128e632a33
Michael Landis <mlandis@gmail.com>
2022-08-15 15:08:44 -0500

    add getRate and getEvents

--- src/revlanguage/datatypes/math/RlCladogeneticSpeciationRateMatrix.cpp
+++ src/revlanguage/datatypes/math/RlCladogeneticSpeciationRateMatrix.cpp
@@ -81,6 +81,30 @@ RevPtr<RevVariable> CladogeneticSpeciationRateMatrix::executeMethod(std::string 
         RevBayesCore::CladogeneticProbabilityMatrix p = this->dag_node->getValue().getCladogeneticProbabilityMatrix();
         return new RevVariable( new CladogeneticProbabilityMatrix( p ) );
     }
+    else if (name == "getRate")
+    {
+        found = true;
+
+        // create state for anc -> ch1, ch2
+        std::vector<unsigned> state;
+        unsigned anc_state = unsigned( static_cast<const Natural&>( args[0].getVariable()->getRevObject() ).getValue() );
+        unsigned ch1_state = unsigned( static_cast<const Natural&>( args[1].getVariable()->getRevObject() ).getValue() );
+        unsigned ch2_state = unsigned( static_cast<const Natural&>( args[2].getVariable()->getRevObject() ).getValue() );
+        state.push_back(anc_state);
+        state.push_back(ch1_state);
+        state.push_back(ch2_state);
+
+        // get rate event map
+        const std::map< std::vector<unsigned>, double >& rates = this->dag_node->getValue().getEventMap();
+        
+        double v = 0.0;
+        auto it = rates.find(state);
+        if (it != rates.end()) {
+            v = it->second;
+        }
+
+        return new RevVariable( new RealPos( v ) );
+    }
     
     return ModelObject<RevBayesCore::CladogeneticSpeciationRateMatrix>::executeMethod( name, args, found );
 }
@@ -118,4 +142,12 @@ void CladogeneticSpeciationRateMatrix::initMethods(void) {
     ArgumentRules* cladogeneticProbabilityMatrixArgRules = new ArgumentRules();
     methods.addFunction( new MemberFunction<CladogeneticSpeciationRateMatrix, CladogeneticProbabilityMatrix>( "getCladogeneticProbabilityMatrix", this, cladogeneticProbabilityMatrixArgRules ) );
 
+    ArgumentRules* getEventsArgRules = new ArgumentRules();
+    methods.addFunction( new MemberFunction<CladogeneticSpeciationRateMatrix, ModelVector<ModelVector<Natural> > >( "getEvents", this, getEventsArgRules ) );
+    
+    ArgumentRules* getRateArgRules = new ArgumentRules();
+    getRateArgRules->push_back(new ArgumentRule( "ancestorState", Natural::getClassTypeSpec(), "The state of the ancestral lineage before cladogenesis.", ArgumentRule::BY_VALUE, ArgumentRule::ANY  ) );
+    getRateArgRules->push_back(new ArgumentRule( "childState1", Natural::getClassTypeSpec(), "The state of the first child lineage after cladogenesis.", ArgumentRule::BY_VALUE, ArgumentRule::ANY  ) );
+    getRateArgRules->push_back(new ArgumentRule( "childState2", Natural::getClassTypeSpec(), "The state of the second child lineage after cladogenesis.", ArgumentRule::BY_VALUE, ArgumentRule::ANY  ) );
+    methods.addFunction( new MemberFunction<CladogeneticSpeciationRateMatrix, RealPos>( "getRate", this, getRateArgRules ) );
 }
