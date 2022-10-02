4db2305523e450dfc57915b2ee2297a832c86e4c
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-10-02 01:02:10 -0400

    Allow specifying the rate to rescale too -- or the lack of such a rate.

--- src/core/datatypes/phylogenetics/ratematrix/ConcreteTimeReversibleRateMatrix.h
+++ src/core/datatypes/phylogenetics/ratematrix/ConcreteTimeReversibleRateMatrix.h
@@ -4,7 +4,7 @@
 #include "TimeReversibleRateMatrix.h"
 #include <complex>
 #include <vector>
-
+#include <boost/optional.hpp>
 
 namespace RevBayesCore {
 
@@ -19,10 +19,11 @@ namespace RevBayesCore {
      */
     class ConcreteTimeReversibleRateMatrix : public TimeReversibleRateMatrix
     {
+        boost::optional<double> _rate;
 
     public:
-        ConcreteTimeReversibleRateMatrix(const std::vector<double>& er, const std::vector<double>& pi);
-        ConcreteTimeReversibleRateMatrix(const MatrixReal& ER, const std::vector<double>& pi);
+        ConcreteTimeReversibleRateMatrix(const std::vector<double>& er, const std::vector<double>& pi, boost::optional<double> rate = 1.0);
+        ConcreteTimeReversibleRateMatrix(const MatrixReal& ER, const std::vector<double>& pi, boost::optional<double> rate = 1.0);
 
         ConcreteTimeReversibleRateMatrix(const ConcreteTimeReversibleRateMatrix& m) = default;
 
