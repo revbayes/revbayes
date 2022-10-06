757110ebae3513fc7c1efa324530f678fd3b9f91
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-10-06 11:24:01 -0400

    Complain if accessing a non-existent child.

--- src/core/datatypes/trees/TopologyNode.cpp
+++ src/core/datatypes/trees/TopologyNode.cpp
@@ -936,6 +936,9 @@ size_t TopologyNode::getCladeIndex(const TopologyNode *c) const
 /** Get child at index i */
 const TopologyNode& TopologyNode::getChild(size_t i) const
 {
+    // Maybe this should be an assert, but not sure what the contract is here.
+    if (i >= children.size())
+        throw RbException()<<"Trying to get child "<<i<<", but node only has "<<children.size()<<" children.";
 
     return *children[i];
 }
@@ -944,6 +947,9 @@ const TopologyNode& TopologyNode::getChild(size_t i) const
 /** Get child at index i */
 TopologyNode& TopologyNode::getChild(size_t i)
 {
+    // Maybe this should be an assert, but not sure what the contract is here.
+    if (i >= children.size())
+        throw RbException()<<"Trying to get child "<<i<<", but node only has "<<children.size()<<" children.";
 
     return *children[i];
 }
