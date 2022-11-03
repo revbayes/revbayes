1e8a371b15d8754e12881a0ad4e15836f5c559ce
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-11-03 06:22:35 -0400

    Also add default tuning methods for Move.

--- src/core/moves/Move.cpp
+++ src/core/moves/Move.cpp
@@ -1,5 +1,10 @@
 #include "Move.h"
 
+#include <cmath>
+
+#include "RbConstants.h"
+#include "RbException.h"
+
 using namespace RevBayesCore;
 
 Move::Move()
@@ -13,6 +18,20 @@ Move::~Move()
     
 }
 
+double Move::getMoveTuningParameter(void) const
+{
+    return RbConstants::Double::nan;
+}
+
+void Move::setMoveTuningParameter(double tp)
+{
+    throw RbException()<<"setProposalTuningParameter: tuning not implemented for "<<getMoveName();
+}
+
+void Move::autoTune(void)
+{
+    throw RbException()<<"autoTune: auto-tuning not implemented for "<<getMoveName();
+}
 
 std::ostream& RevBayesCore::operator<<(std::ostream& o, const Move& x)
 {
--- src/core/moves/Move.h
+++ src/core/moves/Move.h
@@ -30,13 +30,11 @@ template <class valueType> class RbOrderedSet;
         
         // pure virtual public methods
         virtual void                                            addNode(DagNode* p) = 0;                                    //!< add a node to the proposal
-        virtual void                                            autoTune(void) = 0;                                         //!< Automatic tuning of the move.
         virtual Move*                                           clone(void) const = 0;                                      //!< Create a deep copy.
         virtual void                                            decrementTriedCounter(void) = 0;                            //!< Get update weight of InferenceMove
         virtual const RbOrderedSet<DagNode*>&                   getAffectedNodes(void) const = 0;                           //!< Get the nodes vector
         virtual const std::vector<DagNode*>&                    getDagNodes(void) const = 0;                                //!< Get the nodes vector
         virtual const std::string&                              getMoveName(void) const = 0;                                //!< Get the name of the move for summary printing
-        virtual double                                          getMoveTuningParameter(void) const = 0;
         virtual size_t                                          getNumberAcceptedCurrentPeriod(void) const = 0;             //!< Get update weight of InferenceMove
         virtual size_t                                          getNumberAcceptedTotal(void) const = 0;                     //!< Get update weight of InferenceMove
         virtual size_t                                          getNumberTriedCurrentPeriod(void) const = 0;                //!< Get the number of tries for this move since the last reset
@@ -49,13 +47,15 @@ template <class valueType> class RbOrderedSet;
         virtual void                                            printSummary(std::ostream &o, bool current_period) const = 0;                    //!< Print the move summary
         virtual void                                            removeNode(DagNode* p) = 0;                                 //!< remove a node from the proposal
         virtual void                                            resetCounters(void) = 0;                                    //!< Reset the counters such as numTried and numAccepted.
-        virtual void                                            setMoveTuningParameter(double tp) = 0;
         virtual void                                            setNumberAcceptedCurrentPeriod(size_t na) = 0;
         virtual void                                            setNumberAcceptedTotal(size_t na) = 0;
         virtual void                                            setNumberTriedCurrentPeriod(size_t nt) = 0;
         virtual void                                            setNumberTriedTotal(size_t nt) = 0;
         virtual void                                            swapNode(DagNode *oldN, DagNode *newN) = 0;                 //!< Swap the pointers to the variable on which the move works on.
         
+        virtual double                                          getMoveTuningParameter(void) const;
+        virtual void                                            setMoveTuningParameter(double tp);
+        virtual void                                            autoTune(void);                                             //!< Automatic tuning of the move.
         
     protected:
         Move();                                                                                                             //!< Constructor
