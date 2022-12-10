b03ac65941bd4a0bfd88779d8c6a1bcfd7ebdbc2
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2022-12-10 11:26:29 +0100

    catching case when no other trees are good

--- src/core/moves/proposal/tree/GibbsSubtreePruneAndRegraftProposal.cpp
+++ src/core/moves/proposal/tree/GibbsSubtreePruneAndRegraftProposal.cpp
@@ -140,15 +140,18 @@ double GibbsSubtreePruneAndRegraftProposal::doProposal( void )
         tree->restore();
     }
     
+    // pick a new random node according to the weights
+    double u = rng->uniform01() * sum_of_weights;
+    size_t index = 0;
+    
     // sanity check
-    if ( sum_of_weights <= 0.0 )
+    if ( sum_of_weights <= 0.0 || u <= 0.0 )
     {
         failed = true;
         return RbConstants::Double::neginf;
     }
     
-    double u = rng->uniform01() * sum_of_weights;
-    size_t index = 0;
+    // now do the roullette wheel to pick the node index
     while (u > 0.0)
     {
         u -= weights[index];
