bb043f00ee6c3dc5ee6ea38036e99ccca1603338
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-19 21:26:59 -0400

    Allow ConcreteTimeReversibleRateMatrix that is not rescaled.

--- src/core/datatypes/phylogenetics/ratematrix/ConcreteTimeReversibleRateMatrix.cpp
+++ src/core/datatypes/phylogenetics/ratematrix/ConcreteTimeReversibleRateMatrix.cpp
@@ -80,8 +80,8 @@ vector<double> flatten_exchange_rates( const MatrixReal& ER )
 }
 
 /** Construct rate matrix with n states */
-ConcreteTimeReversibleRateMatrix::ConcreteTimeReversibleRateMatrix( const vector<double>& er, const vector<double>& pi)
-    : TimeReversibleRateMatrix( pi.size() )
+ConcreteTimeReversibleRateMatrix::ConcreteTimeReversibleRateMatrix( const vector<double>& er, const vector<double>& pi, boost::optional<double> r)
+    : TimeReversibleRateMatrix( pi.size() ), _rate(r)
 {
     int n = pi.size();
     assert( er.size() == n*(n-1)/2 );
@@ -91,8 +91,8 @@ ConcreteTimeReversibleRateMatrix::ConcreteTimeReversibleRateMatrix( const vector
     update();
 }
 
-ConcreteTimeReversibleRateMatrix::ConcreteTimeReversibleRateMatrix( const MatrixReal& ER, const vector<double>& pi)
-    : ConcreteTimeReversibleRateMatrix( flatten_exchange_rates(ER), pi)
+ConcreteTimeReversibleRateMatrix::ConcreteTimeReversibleRateMatrix( const MatrixReal& ER, const vector<double>& pi, boost::optional<double> r)
+    : ConcreteTimeReversibleRateMatrix( flatten_exchange_rates(ER), pi, r)
 {
 }
 
@@ -133,7 +133,8 @@ void ConcreteTimeReversibleRateMatrix::update( void )
         setDiagonal();
 
         // rescale
-        rescaleToAverageRate( 1.0 );
+        if (_rate)
+            rescaleToAverageRate( *_rate );
 
         // clean flags
         needs_update = false;
