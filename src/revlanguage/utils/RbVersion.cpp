adaefb2c2dfef35b9b8a0a700dd48bb1d2a6a845
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2023-08-04 10:51:28 +0200

    printing BEAGLE info in RevBayes info

--- src/revlanguage/utils/RbVersion.cpp
+++ src/revlanguage/utils/RbVersion.cpp
@@ -9,6 +9,12 @@
 #include <mpi.h>
 #endif
 
+#ifdef RB_BEAGLE
+#include "RbSettings.h"
+#include "BeagleUtilities.h"
+#include "libhmsbeagle/beagle.h"
+#endif
+
 RbVersion::RbVersion( void )
 {
     
@@ -40,7 +46,6 @@ std::string RbVersion::getVersion( void ) const
 
 std::string RbVersion::getHeader( void ) const
 {
-    
     std::string header = "";
     header += "\n";
     header += "RevBayes version (" + getVersion() + ")\n";
@@ -50,6 +55,19 @@ std::string RbVersion::getHeader( void ) const
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     header += "MPI enabled: using " + StringUtilities::to_string(num_processes) + " processes.\n";
 #endif
+#ifdef RB_BEAGLE
+    if ( RbSettings::userSettings().getUseBeagle() == true )
+    {
+        header += "\n";
+        header += "Compiled with BEAGLE library v" + std::string(beagleGetVersion());
+#if defined ( RB_USE_EIGEN3 )
+        header += " using Eigen3 eigensystems ";
+#else
+        header += " using transition matrices ";
+#endif /* RB_USE_EIGEN3 */
+        header += "for parallel likelihood evaluation.\n";
+    }
+#endif /* RB_BEAGLE */
     header += "\n";
     header += "Visit the website www.RevBayes.com for more information about RevBayes.\n";
     header += "\n";
