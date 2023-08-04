1ab9ee395aa3117b57c762d2f2a52e3fc7722392
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2023-08-04 10:59:00 +0200

    BEAGLE utilities

---
+++ src/core/utils/BeagleUtilities.h
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * This file contains declarations of helper functions for BEAGLE.
+ *
+ * @brief Declaration of helper functions for the BEAGLE library.
+ *
+ * (c) Copyright 2009- under GPL version 3
+ * @date Last modified: $Date$
+ * @author Daniel L. Ayres
+ * @license GPL version 3
+ *
+ * $Id$
+ */
+
+#ifndef BeagleUtilities_h
+#define BeagleUtilities_h
+
+#include <string>
+
+namespace RevBayesCore {
+    
+    namespace BeagleUtilities {
+        
+        std::string     printBeagleFlags(long inFlags);     //!< Print BEAGLE capability flags
+        std::string     printErrorCode(int c);              //!< Print the error code for BEAGLE
+
+    }
+}
+
+#endif /* BeagleUtilities_h */
+
