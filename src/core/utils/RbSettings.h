0eeb03806dd88aab183e01e0bc8df7687c405fdc
Sebastian Hoehna <Sebastian.Hoehna@gmail.com>
2023-08-04 10:57:21 +0200

    Add new settings for scaling and peeling.

--- src/core/utils/RbSettings.h
+++ src/core/utils/RbSettings.h
@@ -26,8 +26,10 @@ public:
     const RevBayesCore::path&   getModuleDir(void) const;                           //!< Retrieve the module directory name
     std::string                 getOption(const std::string &k) const;              //!< Retrieve a user option
     size_t                      getOutputPrecision(void) const;                     //!< Retrieve the default output precision width
+    const std::string&          getPartialLikelihoodStoring(void) const;            //!< Retrieve the method for partial likelihood storing
     bool                        getPrintNodeIndex(void) const;                      //!< Retrieve the flag whether we should print node indices
     size_t                      getScalingDensity(void) const;                      //!< Retrieve the scaling density that determines how often to scale the likelihood in CTMC models
+    const std::string&          getScalingMethod(void) const;                       //!< Retrieve the scaling method for the likelihood in CTMC models
     double                      getTolerance(void) const;                           //!< Retrieve the tolerance for comparing doubles
     bool                        getUseScaling(void) const;                          //!< Retrieve the flag whether we should scale the likelihood in CTMC models
     int                         getDebugMCMC(void) const;                           //!< How much work should we perform to check MCMC?
@@ -38,13 +40,14 @@ public:
     void                        setModuleDir(const RevBayesCore::path &md);         //!< Set the module directory name
     void                        setOutputPrecision(size_t p);                       //!< Set the default output precision width
     void                        setOption(const std::string &k, const std::string &v, bool write);  //!< Set the key value pair.
+    void                        setPartialLikelihoodStoring(const std::string s);   //!< Set the method for partial likelihood storing (node|branch|both)
     void                        setPrintNodeIndex(bool tf);                         //!< Set the flag whether we should print node indices
     void                        setScalingDensity(size_t w);                        //!< Set the scaling density n, where CTMC likelihoods are scaled every n-th node (min 1)
+    void                        setScalingMethod(const std::string s);              //!< Set the scaling method, either node or threshold
     void                        setTolerance(double t);                             //!< Set the tolerance for comparing double
     void                        setUseScaling(bool s);                              //!< Set the flag whether we should scale the likelihood in CTMC models
     void                        setDebugMCMC(int d);                                //!< How much work should we perform to check MCMC?
     void                        setLogMCMC(int d);                                  //!< How much logging should we perform to check MCMC?
-    
 private:
     RbSettings(void);                                   //!< Default constructor
     RbSettings(const RbSettings&) = delete;             //!< Prevent copy
@@ -61,6 +64,12 @@ private:
     bool                        useScaling=true;
     int                         debugMCMC = 0;
     int                         logMCMC = 0;
+
+    // related to rescaling to avoid underflow
+    size_t                      scaling_density;
+    bool                        use_scaling;
+    std::string                 scaling_method;
+    std::string                 partial_likelihood_storing;
 };
 
 void showDebug(const std::string& s, int level=1);
