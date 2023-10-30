134e719800e3f90dc4dcf088e27b3328eedf4b1c
binho <binho@binhos-MacBook-Pro.local>
2023-10-30 15:06:03 -0500

    Add forward-time stoch map summarizing functionality

--- src/core/analysis/mcmc/output/JointAncestralStateTrace.h
+++ src/core/analysis/mcmc/output/JointAncestralStateTrace.h
@@ -27,7 +27,7 @@ class Tree;
         Tree*                                       characterMapTree(const Tree &input_summary_tree, int NUM_TIME_SLICES = 500, bool conditional = false, bool joint = false, bool verbose = true);
         Tree*                                       cladoAncestralStateTree(const Tree &inputTree, std::string summary_stat, size_t num_states, int site, bool conditional, bool joint, bool verbose);
         Tree*                                       cladoAncestralStateTree2(const Tree &inputTree, std::string summary_stat, size_t num_states, int site, bool conditional, bool joint, bool verbose);
-        void                                        summarizeCharacterMaps(Tree inputTree, const path& filename, bool verbose, std::string separator);
+        void                                        summarizeCharacterMaps(Tree inputTree, const path& filename, bool verbose, std::string separator, bool bwd_time = true);
 
         size_t                                      getBurnin() const { return burnin; }
         void                                        setBurnin(size_t b);
@@ -37,11 +37,11 @@ class Tree;
         void                                        collectJointAncestralStateSamples(int site, Tree &final_summary_tree, const std::vector<TopologyNode*> &summary_nodes, std::vector<std::vector<double> > &pp_end, std::vector<std::vector<double> > &pp_start, std::vector<std::vector<std::string> > &end_states, std::vector<std::vector<std::string> > &start_states, bool clado, ProgressBar &progress, bool verbose);
         std::string                                 getSiteState( const std::string &site_sample, size_t site );
         void                                        recursivelyCollectAncestralStateSamples(size_t node_index, std::string map_parent_state, bool root, bool conditional, int site, Tree &final_summary_tree, const std::vector<TopologyNode*> &summary_nodes, std::vector<std::vector<double> > &pp_end, std::vector<std::vector<double> > &pp_start, std::vector<double> &pp_clade, std::vector<std::vector<std::string> > &end_states, std::vector<std::vector<std::string> > &start_states, bool clado, ProgressBar &progress, size_t &num_finished_nodes, bool verbose);
-        void                                        recursivelyCollectCharacterMapSamples(size_t node_index, size_t map_parent_state, bool root, bool conditional, Tree &final_summary_tree, const std::vector<TopologyNode*> &summary_nodes, std::vector<std::string> &map_character_history, std::vector<std::string> &map_character_history_posteriors, std::vector<std::string> &map_character_history_shift_prob, ProgressBar &progress, size_t &num_finished_nodes, int NUM_TIME_SLICES, bool verbose);
+        void                                        recursivelyCollectCharacterMapSamples(size_t node_index, size_t map_parent_state, bool root, bool conditional, Tree &final_summary_tree, const std::vector<TopologyNode*> &summary_nodes, std::vector<std::string> &map_character_history, std::vector<std::string> &map_character_history_posteriors, std::vector<std::string> &map_character_history_shift_prob, ProgressBar &progress, size_t &num_finished_nodes, int NUM_TIME_SLICES, bool verbose, bool bwd_time = true);
         void                                        computeMarginalCladogeneticStateProbs(std::vector<double> pp, std::vector<std::string> states, std::vector<double>& best_pp, std::vector<std::string>& best_states);
         void                                        computeMarginalCladogeneticStateProbs2(std::vector<double> pp, std::vector<std::string> states, std::vector<double>& best_pp, std::vector<std::string>& best_states, size_t num_states);
 
-        std::vector< std::pair<size_t, double> >    parseSIMMAPForNode(std::string character_history);
+        std::vector< std::pair<size_t, double> >    parseSIMMAPForNode(std::string character_history, bool bwd_time);
 
         bool                                        usingTreeTrace(void) const { return tree_trace.size() > 0; };
 
