346da82694f2aff1fc17edd20f5a05882a8ede63
Wenjie Zhu <Wenjie.Zhu@campus.lmu.de>
2023-11-16 18:26:17 +0100

    Reorganize MSC-M test data and scripts

--- tests/test_MSC_Migration/scripts/plot.r
+++ tests/test_MSC_Migration/scripts/plot.r
@@ -9,17 +9,19 @@ for (i in 1:length(pow_list)) {
     Ne[i] <- 0.0001 * (10^(pow_list[i]))
 }
 
-MSC <- c(-3822.248, -2406.986, -1514.357, -951.485, -596.677, -373.148, -232.451, -144.017, -88.559, -53.907, -32.383, -19.142, -11.128, -6.411, -3.775, -2.451, -1.956, -1.983, -2.341, -2.906, -3.603)
+MSC <- c(-24.009, -13.858, -7.794, -4.307, -2.447, -1.614, -1.428, -1.650, -2.130, -2.773, -3.519, -4.329, -5.180, -6.057, -6.951, -7.854, -8.764, -9.678, -10.595, -11.513, -12.432)
 
-MSC_M <- c(-23.058, -22.557, -22.771, -23.146, -25.695, -24.636, -28.071, -28.975, -21.751, -21.593, -14.570, -8.422, -4.875, -2.978, -2.120, -1.919, -2.132, -2.606, -3.245, -3.988, -4.797)
+MSC_M <- c(-23.965, -13.858, -7.794, -4.307, -2.447, -1.614, -1.428, -1.650, -2.130, -2.773, -3.519, -4.329, -5.180, -6.057, -6.951, -7.854, -8.764, -9.678, -10.595, -11.513, -12.432)
 
-df <- data.frame(Ne, MSC, MSC_M)
+MSC_InverseGamma <- c(-9.956, -7.772, -5.844, -4.240, -3.015, -2.197, -1.777, -1.711, -1.935, -2.380, -2.984, -3.698, -4.485, -5.320, -6.187, -7.073, -7.972, -8.879, -9.791, -10.707, -11.624)
 
-df
+df <- data.frame(Ne, MSC, MSC_M, MSC_InverseGamma)
 
 # Pivot data from wide to long
 df <- df %>% 
-  pivot_longer(cols = names(df)[2:3], values_to = "lnProbability", names_to = "model") 
+  pivot_longer(cols = names(df)[2:4], values_to = "lnProbability", names_to = "model") 
+
+df
 
 # plot both likelihoods in one figure
 ggplot(df, aes(x = Ne, y = lnProbability, col=model, label=round(lnProbability,1))) +
@@ -28,7 +30,7 @@ ggplot(df, aes(x = Ne, y = lnProbability, col=model, label=round(lnProbability,1
     scale_x_continuous(trans = 'log10') +
     geom_text(size=3, vjust = -2,angle = 45)
 
-ggsave("02_likelihood_msc_vs_mscm/plots/lnProbability.png")
+ggsave("tests/test_MSC_Migration/plots/lnProbability.png")
 
 # plot only the msc-m
 df_mscm <- df %>% filter(model == "MSC_M")
@@ -37,4 +39,4 @@ ggplot(df_mscm , aes(x = Ne, y = lnProbability, label=round(lnProbability,1))) +
     geom_line() +
     scale_x_continuous(trans = 'log10') +
     geom_text(size=3, vjust = -2,angle = 45)
-ggsave("02_likelihood_msc_vs_mscm/plots/lnProbability_mscm.png")
+ggsave("tests/test_MSC_Migration/plots/lnProbability_mscm.png")
