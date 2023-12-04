5411e7202b4a72f1f7f3872e98c9a6bdfe946582
John Huelsenbeck <johnh@berkeley.edu>
2023-12-04 06:11:51 -0800

    Minor changes

--- src/core/moves/proposal/MPQHelpers/Plane.h
+++ src/core/moves/proposal/MPQHelpers/Plane.h
@@ -7,40 +7,39 @@
 
 namespace RevBayesCore {
 
-class Plane {
-    
-    /**
-     * A light-weight class to represent a plane in 3D. Planes are represented in the general
-     * form, with parameters ax + by + cz + d = 0. These plane parameters are GMP rational
-     * numbers.
-     *
-     * @copyright Copyright 2009-
-     * @author The RevBayes Development Core Team (John Huelsenbeck)
-     * @since 2014-11-18, version 1.0
-     */
-    
-public:
-    Plane(void);
-    Plane(Vector pt1, Vector pt2, Vector pt3);
-    Plane(Vector& pt1, Vector& pt2, Vector& pt3);
-    bool                operator==(const Plane& rhs) const;
-    bool                operator<(const Plane& rhs) const;
-    const mpq_class&    getA(void) const { return a; }
-    const mpq_class&    getB(void) const { return b; }
-    const mpq_class&    getC(void) const { return c; }
-    const mpq_class&    getD(void) const { return d; }
-    std::string         getStr(void);
-    void                set(Vector pt1, Vector pt2, Vector pt3);
-    
-private:
-    mpq_class       a;
-    mpq_class       b;
-    mpq_class       c;
-    mpq_class       d;
-    
-    friend std::ostream& operator<<(std::ostream& os, const Plane& pl);
-};
-
+    class Plane {
+        
+        /**
+         * A light-weight class to represent a plane in 3D. Planes are represented in the general
+         * form, with parameters ax + by + cz + d = 0. These plane parameters are GMP rational
+         * numbers.
+         *
+         * @copyright Copyright 2009-
+         * @author The RevBayes Development Core Team (John Huelsenbeck)
+         * @since 2014-11-18, version 1.0
+         */
+        
+    public:
+                            Plane(void);
+                            Plane(Vector pt1, Vector pt2, Vector pt3);
+                            Plane(Vector& pt1, Vector& pt2, Vector& pt3);
+        bool                operator==(const Plane& rhs) const;
+        bool                operator<(const Plane& rhs) const;
+        const mpq_class&    getA(void) const { return a; }
+        const mpq_class&    getB(void) const { return b; }
+        const mpq_class&    getC(void) const { return c; }
+        const mpq_class&    getD(void) const { return d; }
+        std::string         getStr(void);
+        void                set(Vector pt1, Vector pt2, Vector pt3);
+        
+    private:
+        mpq_class           a;
+        mpq_class           b;
+        mpq_class           c;
+        mpq_class           d;
+        
+        friend std::ostream& operator<<(std::ostream& os, const Plane& pl);
+    };
 }
 
 inline std::ostream& operator<<(std::ostream& os, const RevBayesCore::Plane& pl) {
--- src/core/moves/proposal/MPQHelpers/Vector.h
+++ src/core/moves/proposal/MPQHelpers/Vector.h
@@ -8,45 +8,44 @@
 
 namespace RevBayesCore {
 
-class Vector {
-    
-    /**
-     * A light-weight class to represent a vector in 3D, with coordinates x, y, z. The
-     * coordinates are GMP rational numbers.
-     *
-     * @copyright Copyright 2009-
-     * @author The RevBayes Development Core Team (John Huelsenbeck)
-     * @since 2014-11-18, version 1.0
-     */
-    
-public:
-    Vector(void);
-    Vector(const Vector& v);
-    Vector(int xI, int yI, int zI);
-    Vector(double xD, double yD, double zD);
-    Vector(mpq_class& xQ, mpq_class& yQ, mpq_class& zQ);
-    bool            operator==(Vector& rhs);
-    bool            operator!=(Vector& rhs);
-    Vector&         operator+=(Vector& rhs);
-    Vector          cross(Vector& rhs);
-    mpq_class       distanceSquared(const Vector& vec) const;
-    mpq_class&      getX(void) { return x; }
-    mpq_class&      getY(void) { return y; }
-    mpq_class&      getZ(void) { return z; }
-    std::string     getStr(void);
-    double          length(void);
-    void            normalize(void);
-    void            set(mpq_class& xQ, mpq_class& yQ, mpq_class& zQ);
-    void            setX(mpq_class& xQ) { x = xQ; }
-    void            setY(mpq_class& yQ) { y = yQ; }
-    void            setZ(mpq_class& zQ) { z = zQ; }
-    mpq_class       x;
-    mpq_class       y;
-    mpq_class       z;
-    
-    friend std::ostream& operator<<(std::ostream& os, const Vector& pt);
-};
-
+    class Vector {
+        
+        /**
+         * A light-weight class to represent a vector in 3D, with coordinates x, y, z. The
+         * coordinates are GMP rational numbers.
+         *
+         * @copyright Copyright 2009-
+         * @author The RevBayes Development Core Team (John Huelsenbeck)
+         * @since 2014-11-18, version 1.0
+         */
+        
+    public:
+                        Vector(void);
+                        Vector(const Vector& v);
+                        Vector(int xI, int yI, int zI);
+                        Vector(double xD, double yD, double zD);
+                        Vector(mpq_class& xQ, mpq_class& yQ, mpq_class& zQ);
+        bool            operator==(Vector& rhs);
+        bool            operator!=(Vector& rhs);
+        Vector&         operator+=(Vector& rhs);
+        Vector          cross(Vector& rhs);
+        mpq_class       distanceSquared(const Vector& vec) const;
+        mpq_class&      getX(void) { return x; }
+        mpq_class&      getY(void) { return y; }
+        mpq_class&      getZ(void) { return z; }
+        std::string     getStr(void);
+        double          length(void);
+        void            normalize(void);
+        void            set(mpq_class& xQ, mpq_class& yQ, mpq_class& zQ);
+        void            setX(mpq_class& xQ) { x = xQ; }
+        void            setY(mpq_class& yQ) { y = yQ; }
+        void            setZ(mpq_class& zQ) { z = zQ; }
+        mpq_class       x;
+        mpq_class       y;
+        mpq_class       z;
+        
+        friend std::ostream& operator<<(std::ostream& os, const Vector& pt);
+    };
 }
 
 inline std::ostream& operator<<(std::ostream& os, const RevBayesCore::Vector& pt) {
