ba3fd21589ee41576c2a1976f5d3fda6216d7c53
davidcerny <david.cerny1@gmail.com>
2024-07-16 16:46:12 -0500

    Fix an uncaught reference to ExponentialWithOffset

--- src/core/distributions/math/ExponentialDistribution.h
+++ src/core/distributions/math/ExponentialDistribution.h
@@ -24,7 +24,7 @@ template <class valueType> class TypedDagNode;
         
         // public member functions
         double                                              cdf(void) const;                                                  //!< Cummulative density function
-        ExponentialWithOffsetDistribution*                  clone(void) const;                                                //!< Create an independent clone
+        ExponentialDistribution*                            clone(void) const;                                                //!< Create an independent clone
         double                                              computeLnProbability(void);
         double                                              getMax(void) const;
         double                                              getMin(void) const;
