4cb67084f3c0749fd1d7b271e838af0f3e0a80ce
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2024-07-18 16:40:48 +0100

    Commit to AddDistribution<> syntax

--- src/revlanguage/workspace/RbRegister_Dist.cpp
+++ src/revlanguage/workspace/RbRegister_Dist.cpp
@@ -643,12 +643,6 @@ void RevLanguage::Workspace::initializeDistGlobalWorkspace(void)
         AddDistribution< ModelVector<TimeTree>      >( new Dist_Inverse< ModelVector<TimeTree> >());
         AddDistribution< ModelVector< ModelVector<TimeTree> >                  >( new Dist_Inverse< ModelVector<ModelVector<TimeTree> > >());
         AddDistribution< ModelVector<BranchLengthTree>                         >( new Dist_Inverse< ModelVector<BranchLengthTree> >());
-        // AddDistribution< ModelVector<AbstractHomologousDiscreteCharacterData>  >( new Dist_Inverse< ModelVector<AbstractHomologousDiscreteCharacterData> >());
-        
-//        AddDistribution< AbstractHomologousDiscreteCharacterData >( new Dist_phyloCTMCClado() );
-        // addDistribution( new Dist_CTMC() );
-//        addDistribution( new Dist_InversePhylo() );
-
 
         // uniform partitions prior
         AddDistribution< ModelVector<RealPos>       >( new Dist_upp<RealPos>() );
