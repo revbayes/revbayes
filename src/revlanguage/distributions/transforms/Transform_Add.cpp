259e5e35a12e1b69d3dfafdf968d76b0a37218e0
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-07-31 17:20:43 -0400

    Allow number and distribution.

---
+++ src/revlanguage/distributions/transforms/Transform_Add.cpp
@@ -0,0 +1,126 @@
+#include "Transform_Add.h"
+
+#include "ArgumentRule.h"
+#include "ArgumentRules.h"
+#include "Real.h"
+#include "RlSimplex.h"
+#include "StochasticNode.h"
+#include "TypedDistribution.h"
+
+using namespace RevLanguage;
+
+Transform_Add::Transform_Add() : TypedDistribution< Real >(),
+                                     base_distribution( NULL )
+{
+    markAsTransform();
+}
+
+Transform_Add::~Transform_Add()
+{
+}
+
+Transform_Add* Transform_Add::clone( void ) const
+{
+    return new Transform_Add(*this);
+}
+
+RevBayesCore::TransformedDistribution* Transform_Add::createDistribution( void ) const
+{
+    // get the parameters
+    const Distribution& rl_vp                      = static_cast<const Distribution &>( base_distribution->getRevObject() );
+    RevBayesCore::TypedDistribution<double>* vp    = static_cast<RevBayesCore::TypedDistribution<double>* >( rl_vp.createDistribution() );
+
+    RevBayesCore::TypedDagNode<double>* d           = static_cast<const Real &>( delta->getRevObject() ).getDagNode();
+
+    auto add_transform = [=](double x) -> optional<double> { return x + d->getValue(); };
+    auto add_inverse = [=](double x) -> optional<double> { return x - d->getValue(); };
+    auto log_add_prime = [=](double x) -> optional<double> { return 0; };
+
+    RevBayesCore::TransformedDistribution* dist = new RevBayesCore::TransformedDistribution(*vp, add_transform, add_inverse, log_add_prime, {d});
+
+    delete vp;
+
+    return dist;
+}
+
+
+/* Get Rev type of object */
+const std::string& Transform_Add::getClassType(void)
+{
+
+    static std::string rev_type = "Transform_Add";
+
+    return rev_type;
+}
+
+/* Get class type spec describing type of object */
+const TypeSpec& Transform_Add::getClassTypeSpec(void)
+{
+    static TypeSpec rev_type_spec = TypeSpec( getClassType(), new TypeSpec( TypedDistribution< Real >::getClassTypeSpec() ) );
+
+    return rev_type_spec;
+}
+
+
+/**
+ * Get the Rev name for the distribution.
+ * This name is used for the constructor and the distribution functions,
+ * such as the density and random value function
+ *
+ * \return Rev name of constructor function.
+ */
+std::string Transform_Add::getDistributionFunctionName( void ) const
+{
+    // create a distribution name variable that is the same for all instance of this class
+    std::string d_name = "_add";
+
+    return d_name;
+}
+
+
+/** Return member rules (no members) */
+const MemberRules& Transform_Add::getParameterRules(void) const
+{
+    static MemberRules dist_member_rules;
+    static bool rules_set = false;
+
+    if ( rules_set == false )
+    {
+	std::vector<TypeSpec> distTypes = { TypedDistribution<Real>::getClassTypeSpec(), TypedDistribution<RealPos>::getClassTypeSpec(), TypedDistribution<Probability>::getClassTypeSpec()};
+
+        dist_member_rules.push_back( new ArgumentRule( "baseDistribution", distTypes, "The distribution to be transformed.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        dist_member_rules.push_back( new ArgumentRule( "delta", Real::getClassTypeSpec()   , "The amount added to base random variable.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+
+        rules_set = true;
+    }
+
+    return dist_member_rules;
+}
+
+
+const TypeSpec& Transform_Add::getTypeSpec( void ) const
+{
+    static TypeSpec ts = getClassTypeSpec();
+
+    return ts;
+}
+
+
+
+/** Set a member variable */
+void Transform_Add::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
+{
+    if ( name == "baseDistribution" )
+    {
+        base_distribution = var;
+    }
+    else if ( name == "delta" )
+    {
+        delta = var;
+    }
+    else
+    {
+        TypedDistribution< Real >::setConstParameter(name, var);
+    }
+}
+
