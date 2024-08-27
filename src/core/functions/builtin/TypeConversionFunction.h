243746b817c8d1b8b526bef007184d39a7b447b8
bjoelle <barido@bio.ens.psl.eu>
2024-08-27 16:31:34 +0200

    correct string conversion

--- src/core/functions/builtin/TypeConversionFunction.h
+++ src/core/functions/builtin/TypeConversionFunction.h
@@ -67,22 +67,19 @@ void  RevBayesCore::TypeConversionFunction<rbTypeFrom, rbTypeTo>::swapParameterI
     
 }
 
-
-/**
- * The clone function is a convenience function to create proper copies of inherited objected.
- * E.g. a.clone() will create a clone of the correct type even if 'a' is of derived type 'b'.
- *
- */
 template <typename rbTypeFrom, typename rbTypeTo>
-void RevBayesCore::TypeConversionFunction<rbTypeFrom, rbTypeTo>::update( void )
+void RevBayesCore::TypeConversionFunction<rbTypeFrom, rbTypeTo>::update(void)
 {
-    
-    *(this->value) = rbTypeTo ( argumentNode->getValue() );
-    
+    if constexpr (std::is_same_v<rbTypeTo, std::string>)
+    {
+        std::ostringstream o;
+        argumentNode -> printValue( o, "", -1, true, true, true );
+        *(this->value) = (o.str());
+    }
+    else
+    {
+        *(this->value) = rbTypeTo(argumentNode->getValue());
+    }
 }
 
-
-
 #endif
-
-
