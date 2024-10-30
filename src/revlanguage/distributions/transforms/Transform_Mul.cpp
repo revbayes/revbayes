717f54da0c314536908834a67c9e30e75a8e8699
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-10-29 21:46:41 -0400

    Add Transform_Mul.

---
+++ src/revlanguage/distributions/transforms/Transform_Mul.cpp
@@ -0,0 +1,126 @@
+#include "Transform_Mul.h"
+
+#include "ArgumentRule.h"
+#include "ArgumentRules.h"
+#include "Real.h"
+#include "RlSimplex.h"
+#include "StochasticNode.h"
+#include "TypedDistribution.h"
+
+using namespace RevLanguage;
+
+Transform_Mul::Transform_Mul() : TypedDistribution< Real >(),
+                                     base_distribution( NULL )
+{
+    markAsTransform();
+}
+
+Transform_Mul::~Transform_Mul()
+{
+}
+
+Transform_Mul* Transform_Mul::clone( void ) const
+{
+    return new Transform_Mul(*this);
+}
+
+RevBayesCore::TransformedDistribution* Transform_Mul::createDistribution( void ) const
+{
+    // get the parameters
+    const Distribution& rl_vp                      = static_cast<const Distribution &>( base_distribution->getRevObject() );
+    RevBayesCore::TypedDistribution<double>* vp    = static_cast<RevBayesCore::TypedDistribution<double>* >( rl_vp.createDistribution() );
+
+    RevBayesCore::TypedDagNode<double>* d           = static_cast<const Real &>( delta->getRevObject() ).getDagNode();
+
+    auto mul_transform = [=](double x) -> optional<double> { return d->getValue() * x; };
+    auto mul_inverse = [=](double x) -> optional<double> { return d->getValue() / x; };
+    auto log_mul_prime = [=](double x) -> optional<double> { return log(abs(d->getValue())); };
+
+    RevBayesCore::TransformedDistribution* dist = new RevBayesCore::TransformedDistribution(*vp, mul_transform, mul_inverse, log_mul_prime, {d});
+
+    delete vp;
+
+    return dist;
+}
+
+
+/* Get Rev type of object */
+const std::string& Transform_Mul::getClassType(void)
+{
+
+    static std::string rev_type = "Transform_Mul";
+
+    return rev_type;
+}
+
+/* Get class type spec describing type of object */
+const TypeSpec& Transform_Mul::getClassTypeSpec(void)
+{
+    static TypeSpec rev_type_spec ( TypedDistribution< Real >::getClassTypeSpec() );
+
+    return rev_type_spec;
+}
+
+
+/**
+ * Get the Rev name for the distribution.
+ * This name is used for the constructor and the distribution functions,
+ * such as the density and random value function
+ *
+ * \return Rev name of constructor function.
+ */
+std::string Transform_Mul::getDistributionFunctionName( void ) const
+{
+    // create a distribution name variable that is the same for all instance of this class
+    std::string d_name = "_mul";
+
+    return d_name;
+}
+
+
+/** Return member rules (no members) */
+const MemberRules& Transform_Mul::getParameterRules(void) const
+{
+    static MemberRules dist_member_rules;
+    static bool rules_set = false;
+
+    if ( rules_set == false )
+    {
+	std::vector<TypeSpec> distTypes = { TypedDistribution<Real>::getClassTypeSpec(), TypedDistribution<RealPos>::getClassTypeSpec(), TypedDistribution<Probability>::getClassTypeSpec()};
+
+        dist_member_rules.push_back( new ArgumentRule( "baseDistribution", distTypes, "The distribution to be transformed.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+        dist_member_rules.push_back( new ArgumentRule( "delta", Real::getClassTypeSpec()   , "The amount muled to base random variable.", ArgumentRule::BY_CONSTANT_REFERENCE, ArgumentRule::ANY ) );
+
+        rules_set = true;
+    }
+
+    return dist_member_rules;
+}
+
+
+const TypeSpec& Transform_Mul::getTypeSpec( void ) const
+{
+    static TypeSpec ts = getClassTypeSpec();
+
+    return ts;
+}
+
+
+
+/** Set a member variable */
+void Transform_Mul::setConstParameter(const std::string& name, const RevPtr<const RevVariable> &var)
+{
+    if ( name == "baseDistribution" )
+    {
+        base_distribution = var;
+    }
+    else if ( name == "delta" )
+    {
+        delta = var;
+    }
+    else
+    {
+        TypedDistribution< Real >::setConstParameter(name, var);
+    }
+}
+
