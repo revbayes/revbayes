a35c891b8fad90e27ee29cc91b49acfcb076857f
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-11-06 18:29:48 -0500

    Change number of generations to std::uint64_t

--- src/core/moves/MoveSchedule.h
+++ src/core/moves/MoveSchedule.h
@@ -1,7 +1,7 @@
 #ifndef MoveSchedule_H
 #define MoveSchedule_H
 
-
+#include <cstdint>
 
 namespace RevBayesCore {
 class Move;
@@ -15,7 +15,7 @@ template <class valueType> class RbVector;
         // pure virtual public methods
         virtual MoveSchedule*                                   clone(void) const = 0;
         virtual double                                          getNumberMovesPerIteration(void) const = 0;
-        virtual Move&                                           nextMove(unsigned long g) = 0;
+        virtual Move&                                           nextMove(std::uint64_t g) = 0;
         
         // public methods
         void                                                    tune(void);                                                                                     //!< The the moves to achieve better performance.
