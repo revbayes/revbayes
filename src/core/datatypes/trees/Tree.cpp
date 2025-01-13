ba6ae9b3451a3fd0e402e64a02291672bb224c8f
davidcerny <david.cerny1@gmail.com>
2025-01-12 21:27:22 -0600

    Add a caveat to source comment

--- src/core/datatypes/trees/Tree.cpp
+++ src/core/datatypes/trees/Tree.cpp
@@ -1985,7 +1985,8 @@ void Tree::resolveMultifurcations( bool resolve_root )
         }
     }
     
-    // eliminate any outdegree-1 nodes
+    // Remove the outdegree-1 nodes ("knuckles") introduced by the above process of resolving multifurcations.
+    // NOTE: This only works if we assume that trees can contain multifurcations or knuckles, but not both.
     suppressOutdegreeOneNodes( false );
 }
 
