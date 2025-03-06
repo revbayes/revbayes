6d8d37bcd237429fc5cddaf36bde7bd89370b3b8
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-03-06 17:10:50 -0500

    Move getNodePrs( ) and compareNodePrs( ) into DebugMove.{h,cpp}

--- src/core/moves/MetropolisHastingsMove.cpp
+++ src/core/moves/MetropolisHastingsMove.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "DagNode.h"
+#include "DebugMove.h"
 #include "MetropolisHastingsMove.h"
 #include "Proposal.h"
 #include "RandomNumberFactory.h"
@@ -256,61 +257,6 @@ void MetropolisHastingsMove::performHillClimbingMove( double lHeat, double pHeat
 
     }
 
-}
-
-std::map<const DagNode*, double> getNodePrs(const std::vector<DagNode*>& nodes, const RbOrderedSet<DagNode*>& affected_nodes)
-{
-    std::map<const DagNode*, double> Prs;
-    for(auto node: views::concat(nodes, affected_nodes))
-	Prs.insert({node, node->getLnProbability()});
-    return Prs;
-}
-
-constexpr double rel_err_threshhold = 1.0e-11;
-constexpr int err_precision = 11;
-
-void compareNodePrs(const std::string& name, const std::map<const DagNode*, double>& pdfs1, const std::map<const DagNode*, double>& pdfs2, const std::string& msg)
-{
-    RbException E;
-    E<<std::setprecision(err_precision)<<"Executing "<<name<<": "<<msg<<"!\n";
-    bool err = false;
-    bool weird = false;
-    for(auto& [node,pr1]: pdfs1)
-    {
-	auto pr2 = pdfs2.at(node);
-
-	// If they are equal then there is no difference.
-	if (pr1 == pr2 or (std::isnan(pr1) and std::isnan(pr2)))
-        {
-            // But if both are NaN or -Inf then its kind of weird!
-            // This could indicate that a previous move created a -Inf situation.
-            // In the future it could indicate that we haven't yet moved from Pr=0 to Pr>0.
-            if (not std::isfinite(pr1))
-            {
-                std::cerr<<"    WEIRD: "<<node->getName()<<": "<<pr1<<"\n";
-                weird = true;
-            }
-            continue;
-        }
-
-	// Be a bit careful about computing a relative error.
-	double abs_err = std::abs(pr1 - pr2);
-	double scale = std::min(std::abs(pr1),std::abs(pr2));
-	if (scale < 1 or not RbMath::isAComputableNumber(scale))
-	    scale = 1;
-	double rel_err = abs_err/scale;
-
-	// Complain if rel_err is NaN.
-	if (not (rel_err < rel_err_threshhold))
-	{
-	    E<<"    "<<node->getName()<<": "<<pr1<<" != "<<pr2<<"    diff = "<<pr1-pr2<<"\n";
-	    err = true;
-	}
-    }
-
-    if (weird) std::cerr<<std::endl;
-
-    if (err) throw E;
 }
 
 void MetropolisHastingsMove::performMcmcMove( double prHeat, double lHeat, double pHeat )
