27d2b5583cfb24b1ff995244f464818f7f8fc594
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-05-31 17:14:29 -0400

    Add pdNormal(mu,sigma)

--- src/revlanguage/workspace/RbRegister_Func.cpp
+++ src/revlanguage/workspace/RbRegister_Func.cpp
@@ -320,6 +320,7 @@
 #include "Func_PseudoDataAbove.h"
 #include "Func_PseudoDataBelow.h"
 #include "Func_PseudoDataAnd.h"
+#include "Func_PseudoDataNormal.h"
 
 /* Type conversions */
 #include "Proc_StringToInt.h"
@@ -673,6 +674,7 @@ void RevLanguage::Workspace::initializeFuncGlobalWorkspace(void)
         addFunction( new Func_PseudoDataAbove()                     );
         addFunction( new Func_PseudoDataBelow()                     );
         addFunction( new Func_PseudoDataAnd<Real>()                 );
+        addFunction( new Func_PseudoDataNormal()                    );
 
         // Type conversion
         addFunction( new Proc_StringToInt( )                         );
