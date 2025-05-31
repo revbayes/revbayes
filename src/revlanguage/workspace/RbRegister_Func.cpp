fe33743d39e6dea7b7b195489edf5138c8b801aa
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-05-31 12:57:13 -0400

    Add pdBelow

--- src/revlanguage/workspace/RbRegister_Func.cpp
+++ src/revlanguage/workspace/RbRegister_Func.cpp
@@ -320,6 +320,7 @@
 #include "Func_PseudoObservation.h"
 #include "Func_PseudoDataBetween.h"
 #include "Func_PseudoDataAbove.h"
+#include "Func_PseudoDataBelow.h"
 
 /* Type conversions */
 #include "Proc_StringToInt.h"
@@ -673,6 +674,7 @@ void RevLanguage::Workspace::initializeFuncGlobalWorkspace(void)
         addFunction( new Func_PseudoObservation()                   );
         addFunction( new Func_PseudoDataBetween()                   );
         addFunction( new Func_PseudoDataAbove()                     );
+        addFunction( new Func_PseudoDataBelow()                     );
 
         // Type conversion
         addFunction( new Proc_StringToInt( )                         );
