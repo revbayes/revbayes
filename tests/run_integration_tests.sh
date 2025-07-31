e565f3474a01a5742105d16c72a978ed943066f9
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-07-31 16:37:13 -0400

    Allow specifying what tests to run.

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -31,10 +31,12 @@ while echo $1 | grep ^- > /dev/null; do
     # intercept help while parsing "-key value" pairs
     if [ "$1" = "--help" ] || [ "$1" = "-h" ]
     then
-        echo '
+        echo "USAGE: $0 [options] rb [args] [--tests test1 ...]
+
 Command line options are:
--h                              : print this help and exit.
-'
+-h, --help                              : print this help and exit.
+-t, --tests test1 [test2 ...]           : only run these tests
+"
         exit
     fi
 
@@ -44,24 +46,30 @@ Command line options are:
     shift
 done
 
-if [ $mpi = "true" ]; then
-#    rb_exec="mpirun --oversubscribe -np 4 $@"
-#    rb_exec="mpirun -np 4 $@"
-    rb_exec="$@"
-else
-    rb_exec="$@"
-fi
+rb_exec=()
+only_tests=()
+found_tests=0
 
-export rb_exec
+for arg in "$@" ; do
+    if [[ "$found_tests" = 0 && "$arg" = "--tests" ]] ; then
+        found_tests=1
+    elif [[ "$found_tests" -eq 1 ]]; then
+        only_tests+=("test_$arg")
+    else
+        rb_exec+=("$arg")
+    fi
+done
 
 
-if ! ${rb_exec} --help > /dev/null 2>&1 ; then
-    echo "RevBayes command '${rb_exec}' seems not to work!"
+# if [ $mpi = "true" ]; then
+#    rb_exec="mpirun --oversubscribe -np 4 ${rb_exec}"
+#    rb_exec="mpirun -np 4 ${rb_exec}"
+# fi
+
+if [ "$(${rb_exec[@]} -e '1+2' 2>/dev/null)" != "   3" ] ; then
+    echo "RevBayes command '${rb_exec[@]}' seems not to work!"
     exit 102
 fi
-
-tests=()
-status=()
 
 if [ ! -d "revbayes.github.io" ] ; then
     echo "No revbayes.github.io directory, cloning it"
@@ -69,19 +77,32 @@ if [ ! -d "revbayes.github.io" ] ; then
 fi
 
 # Run the tutorial tests using the script from the website
+if [ ${#only_tests[@]} = 0 ] ; then
 (
     cd revbayes.github.io/tutorials
-    ./run_tutorial_tests.sh ${rb_exec}
+    ./run_tutorial_tests.sh ${rb_exec[@]}
 )
+fi
 
-for t in test_*; do
+if [ ${#only_tests[@]} -gt 0 ] ; then
+    TESTS=${only_tests[@]}
+    echo "Only running tests: ${only_tests[@]}"
+else
+    TESTS=$(echo test_*)
+fi
+
+tests=()
+status=()
+
+for t in ${TESTS}; do
     testname=`echo $t | cut -d _ -f 2-`
 
-    if [ -d $t ]; then
-        tests+=($testname)
-    else
-        continue
+    if [ ! -d $t ]; then
+        echo "directory $t does not exist!"
+        exit 1
     fi
+
+    tests+=($testname)
 
     printf "\n${BOLD}#### Running test: ${CLEAR}${UNDERLINE}$testname${CLEAR}\n"
     cd $t
@@ -96,7 +117,7 @@ for t in test_*; do
         mkdir -p output
         tmp0=${f#scripts/}
         script_name=${tmp0%.[Rr]ev}
-        ${rb_exec} $f &> output/${script_name}.errout # print output so we can see any error messages
+        ${rb_exec[@]} $f &> output/${script_name}.errout # print output so we can see any error messages
         script_result="$?"
 
         expected_exit=0
@@ -145,53 +166,56 @@ i=0
 while [  $i -lt ${#tests[@]} ]; do
     t=${tests[$i]}
 
-    if [ -d test_$t ]; then
-        cd test_$t
+    if [ ! -d "test_$t" ]; then
+        echo "directory test_$t not found!"
+        exit 1
+    fi
 
-        # check if output matches expected output
-        errs=()
-        exp_out_dir="output_expected"
-        # Sebastian: For now we only use single cores until we fix Travis mpirun
+    cd "test_$t"
+
+    # check if output matches expected output
+    errs=()
+    exp_out_dir="output_expected"
+    # Sebastian: For now we only use single cores until we fix Travis mpirun
     #    if [ "${mpi}" = "true" ]; then
     #        if [ -d "output_expected_mpi" ]; then
     #            exp_out_dir="output_expected_mpi"
     #        fi
     #    fi
-        if [ "$windows" = "true" ]; then
-            find output -type f -exec dos2unix {} \;
-            find output -type f -exec sed -i 's/e-00/e-0/g' {} \;
-            find output -type f -exec sed -i 's/e+00/e+0/g' {} \;
-        fi
-
-        # some special handling for the *.errout files
-        for f in scripts/*.[Rr]ev ; do
-            tmp0=${f#scripts/}
-            script_name=${tmp0%.[Rr]ev}
-
-            # Delete the final line of failing tests, which reprints the path to the script
-            # that differs between Windows and Unix (has no effect if the line is absent)
-            sed '/   Error:\tProblem processing/d' output/${script_name}.errout > output/${script_name}.errout.tmp
-            mv output/${script_name}.errout.tmp output/${script_name}.errout
-
-            # Account for OS-specific differences in path separators
-            if [ "$windows" = "true" ]; then
-                sed 's/\\/\//g' output/${script_name}.errout > output/${script_name}.errout.tmp
-                mv output/${script_name}.errout.tmp output/${script_name}.errout
-            fi
-        done
-
-        for expected in $(find output_expected -type f 2>/dev/null); do
-            filename=${expected#output_expected/}
-            output=output/${filename}
-            if [ ! -e "${output}" ]; then
-                errs+=("missing:  ${filename}")
-            elif ! diff "${output}" "${expected}" > /dev/null; then
-                errs+=("mismatch: ${filename}")
-            fi
-        done
-
-        cd ..
+    if [ "$windows" = "true" ]; then
+        find output -type f -exec dos2unix {} \;
+        find output -type f -exec sed -i 's/e-00/e-0/g' {} \;
+        find output -type f -exec sed -i 's/e+00/e+0/g' {} \;
     fi
+
+    # some special handling for the *.errout files
+    for f in scripts/*.[Rr]ev ; do
+        tmp0=${f#scripts/}
+        script_name=${tmp0%.[Rr]ev}
+
+        # Delete the final line of failing tests, which reprints the path to the script
+        # that differs between Windows and Unix (has no effect if the line is absent)
+        sed '/   Error:\tProblem processing/d' output/${script_name}.errout > output/${script_name}.errout.tmp
+        mv output/${script_name}.errout.tmp output/${script_name}.errout
+
+        # Account for OS-specific differences in path separators
+        if [ "$windows" = "true" ]; then
+            sed 's/\\/\//g' output/${script_name}.errout > output/${script_name}.errout.tmp
+            mv output/${script_name}.errout.tmp output/${script_name}.errout
+        fi
+    done
+
+    for expected in $(find output_expected -type f 2>/dev/null); do
+        filename=${expected#output_expected/}
+        output=output/${filename}
+        if [ ! -e "${output}" ]; then
+            errs+=("missing:  ${filename}")
+        elif ! diff -u "${expected}" "${output}" > /dev/null ; then
+            errs+=("mismatch: ${filename}")
+        fi
+    done
+
+    cd ..
 
     # check if a script exited with an error
     if [ "${status[$i]}" != 0 ]; then
