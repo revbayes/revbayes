5c0f21e158dad1b326fcfe2a058d6c8db1356600
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-09-18 14:47:30 -0400

    Use LogDensity in DuplicateLossProcess

--- src/core/distributions/phylogenetics/tree/DuplicationLossProcess.h
+++ src/core/distributions/phylogenetics/tree/DuplicationLossProcess.h
@@ -38,8 +38,8 @@ namespace RevBayesCore {
         void                                                buildRandomBinaryTree(std::vector<TopologyNode *> &tips);
         double                                              computeD(double dt, double e);
         double                                              computeE(double dt, double e);
-        double                                              computeLnDuplicationLossProbability(size_t k, const std::vector<double> &t, double a, double b, const TopologyNode &n, bool f);
-        double                                              recursivelyComputeLnProbability(const TopologyNode &n);
+        LogDensity                                          computeLnDuplicationLossProbability(size_t k, const std::vector<double> &t, double a, double b, const TopologyNode &n, bool f);
+        LogDensity                                          recursivelyComputeLnProbability(const TopologyNode &n);
         void                                                resetTipAllocations(void);
         void                                                simulateTree(void);
         
