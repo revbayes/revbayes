e21ead75856bc88d2934696be717dce1909f6ad2
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-09-18 11:42:32 -0400

    Add notes and draft implementation for whitespace

--- src/core/io/NewickConverter.cpp
+++ src/core/io/NewickConverter.cpp
@@ -13,6 +13,7 @@
 using std::optional;
 using std::pair;
 using std::string;
+using std::vector;
 
 namespace RevBayesCore
 {
@@ -104,39 +105,20 @@ Tree* NewickConverter::convertFromNewick(std::string const &n)
 Comments:
 
     - We could simplify by doing `Subtree -> [ "(" BranchSet ")" ] Name`, where [..] means optional.
+    - Newick comments are considered whitespace.  They can go anywhere whitespace can go.
+    - If we explicitly consider whitespace, then
+
+          Leaf -> Name
+
+      looks like
+
+          Leaf -> Whitespace Name Whitespace
+      
+      This is a bit uglier to read.
+          
     - ???
 
 */
-
-// Tree -> Subtree ";"
-std::optional<std::pair<TopologyNode*, int>> parseTree(const std::string& input, int start_pos)
-{
-    // 1. Get the Subtree
-    auto check_subtree = parseSubTree(input,start_pos);
-    if (not check_subtree)
-        return {};
-
-    auto& [subtree, new_start_pos] = *check_subtree;
-
-    auto check_semi = checkChar(input, new_start_pos, ';');
-    if (not check_semi)
-        return {};
-
-    int new_start_pos2 = *check_semi;
-
-    return {{subtree, new_start_pos2}};
-}
-
-// subtree -> internal OR leaf
-std::optional<std::pair<TopologyNode*, int>> parseSubTree(const std::string& input, int start_pos){
-
-    if (auto check = parseInternal(input, start_pos))
-        return check;
-    
-    else {
-        return parseLeaf(input, start_pos);
-    }
-}
 
 // succeeds if there is another character in the input, otherwise fails
 std::optional<std::pair<char, int>> parseChar(const std::string& input, int start_pos){
@@ -162,6 +144,118 @@ std::optional<int> checkChar(const std::string& input, int start_pos, char c){
     }
     else{
         return {};
+    }
+}
+
+
+optional<pair<string,int>> parseNewickComment(const std::string& input, int start_pos)
+{
+    // 1. First check for an open bracket
+    auto check_open_bracket = checkChar(input, start_pos, '[');
+    if (not check_open_bracket)
+        return {};
+
+    // FIXME: only some comments are supposed to be computer-readable.
+    // Comments that start with [& should be saved.
+    // Also some others... [%U, [%R, and [&&NHX:
+    // Currently we don't try to decide here what to save and what to discard, and just pass back all comments.
+
+    // 2. Then read characters until an end bracket
+    std::string newick_comment;
+    while(auto check_char = parseChar(input, start_pos))
+    {
+        auto& [c, new_start_pos] = *check_char;
+        start_pos = new_start_pos;
+        if (c != ']')
+            newick_comment += 'c';
+        else
+            return {{newick_comment, start_pos}};
+    }
+
+    // 3. If we get here, then ending bracket is missing!
+    return {};
+}
+
+optional<pair<char,int>> parseWhiteChar(const std::string& input, int start_pos)
+{
+    // 1. Try to read a character
+    auto check_char = parseChar(input, start_pos);
+    if (not check_char) return {};
+    auto& [c, new_start_pos] = *check_char;
+
+    // 2. If it is whitespace, return the character
+    if (c == ' ' or c == '\t' or c == '\n' or c == '\r')
+        return {{c, new_start_pos}};
+    // 3. If it is not whitespace, return null
+    else
+        return {};
+
+    // QUESTION: Which characters should count as whitespace?
+}
+    
+// OneWhitespace -> NewickComment OR WhiteChar    
+optional<pair<optional<string>,int>> parseOneWhitespace(const std::string& input, int start_pos)
+{
+    // 1. First try to read a newick comment
+    if (auto check_comment = parseNewickComment(input, start_pos))
+    {
+        auto& [comment, new_start_pos] = *check_comment;
+        return {{{comment}, new_start_pos}};
+    }
+    // 2. Then try to read a whitespace character
+    else if (auto check_char = parseWhiteChar(input, start_pos))
+    {
+        auto& [c, new_start_pos] = *check_char;
+        // If we found a whitespace char, return success with no comment
+        return {{{},new_start_pos}};
+    }
+    // 3. If no comment and no whitespace character, return null
+    else
+        return {};
+}
+
+// Whitespace -> OneWhitespace*
+optional<pair<vector<string>,int>> parseWhitespace(const std::string& input, int start_pos)
+{
+    vector<string> newick_comments;
+    while(auto check = parseOneWhitespace(input, start_pos))
+    {
+        auto& [maybe_comment,new_start_pos] = *check;
+        start_pos = new_start_pos;
+        if (maybe_comment)
+            newick_comments.push_back(*maybe_comment);
+    }
+    return {{newick_comments, start_pos}};
+}
+
+
+// Tree -> Subtree ";"
+std::optional<std::pair<TopologyNode*, int>> parseTree(const std::string& input, int start_pos)
+{
+    // 1. Get the Subtree
+    auto check_subtree = parseSubTree(input,start_pos);
+    if (not check_subtree)
+        return {};
+    auto& [subtree, new_start_pos] = *check_subtree;
+
+    // 2. Check the semicolon
+    auto check_semi = checkChar(input, new_start_pos, ';');
+    if (not check_semi)
+        return {};
+
+    int new_start_pos2 = *check_semi;
+
+    return {{subtree, new_start_pos2}};
+}
+
+// subtree -> internal OR leaf
+std::optional<std::pair<TopologyNode*, int>> parseSubTree(const std::string& input, int start_pos){
+
+    if (auto check = parseInternal(input, start_pos))
+        return check;
+    
+    else {
+        return parseLeaf(input, start_pos);
     }
 }
 
