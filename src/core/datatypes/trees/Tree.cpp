3b298d46fc42851bd30da1ae02d8e7db8a67d75c
brpetrucci <petrucci@iastate.edu>
2025-10-03 13:05:50 -0500

    Made posterior branch comment

--- src/core/datatypes/trees/Tree.cpp
+++ src/core/datatypes/trees/Tree.cpp
@@ -2081,24 +2081,10 @@ TopologyNode& Tree::reverseParentChild(TopologyNode &n)
         TopologyNode &p = n.getParent();
         ret = &(reverseParentChild(p));
 
-        // save p values
-        size_t idx_p = p.getIndex();
-        double brlen_p = p.getBranchLength();
-        std::vector<std::string> ncomm_p = p.node_comments;
-        std::vector<std::string> bcomm_p = p.branch_comments;
-
-        // we need to re-orient the branches/indices so that
-        // nodes remain associated with the same parameters
-        p.setIndex(n.getIndex());
-        p.setBranchLength(n.getBranchLength());
-        p.node_comments = n.node_comments;
-        p.branch_comments = n.branch_comments;
-
-        // set n values
-        n.setIndex(idx_p);
-        n.setBranchLength(brlen_p);
-        n.node_comments = ncomm_p;
-        n.branch_comments = bcomm_p;
+        // swap indices, branch lengths, and branch comments
+        // std::swap(p.index, n.index);
+        std::swap(p.branch_length, n.branch_length);
+        std::swap(p.branch_comments, n.branch_comments);
 
         p.removeChild( &n );
         n.setParent( nullptr ); // Avoid loop in parent edges from n -> p -> n
