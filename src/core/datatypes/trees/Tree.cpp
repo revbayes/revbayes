4087536f3ef9e798214bd05fa8e2668ee98d1bc5
brpetrucci <petrucci@iastate.edu>
2025-10-03 13:29:40 -0500

    And change the comment itself

--- src/core/datatypes/trees/Tree.cpp
+++ src/core/datatypes/trees/Tree.cpp
@@ -2081,7 +2081,7 @@ TopologyNode& Tree::reverseParentChild(TopologyNode &n)
         TopologyNode &p = n.getParent();
         ret = &(reverseParentChild(p));
 
-        // swap indices, branch lengths, and branch comments
+        // swap branch lengths and branch comments
         std::swap(p.branch_length, n.branch_length);
         std::swap(p.branch_comments, n.branch_comments);
 
