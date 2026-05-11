f4b518e1cafc5f1f9973e4d213f2de34acdb2c48
davidcerny <david.cerny1@gmail.com>
2026-05-12 00:44:10 +0200

    Fix RNA alignments

--- src/core/datatypes/phylogenetics/character/RnaState.cpp
+++ src/core/datatypes/phylogenetics/character/RnaState.cpp
@@ -21,6 +21,20 @@ RnaState::RnaState(const std::string &s) : DiscreteCharacterState( 4 )
 }
 
 
+RnaState::RnaState(const RbBitSet& bs)  : DiscreteCharacterState( 4)
+{
+    int mask = 0;
+    if (bs[0]) mask |= 1;
+    if (bs[1]) mask |= 2;
+    if (bs[2]) mask |= 4;
+    if (bs[3]) mask |= 8;
+
+    const char* states = " ACMGRSVUWYHKDBN";
+
+    state = states[mask];
+}
+
+
 RnaState* RnaState::clone( void ) const
 {
     
@@ -28,14 +42,187 @@ RnaState* RnaState::clone( void ) const
 }
 
 
-
-void RnaState::setState(const std::string &symbol)
+bool RnaState::operator==(const CharacterState& x) const
 {
-    
-    char s = char( toupper( symbol[0] ) );
-    state = s;
-
+    if (auto d = dynamic_cast<const RnaState*>(&x))
+        return operator==(*d);
+    else
+        return false;
 }
+
+
+bool RnaState::operator==(const RnaState& x) const
+{
+    return state == x.state;
+}
+
+
+void RnaState::operator+=(int i)
+{
+    setStateByIndex(getStateIndex()+i);
+}
+
+
+void RnaState::operator-=(int i)
+{
+    setStateByIndex(getStateIndex()-i);
+}
+
+
+void RnaState::addState(const std::string &symbol)
+{
+    char s = char( toupper( symbol[0] ) );
+    
+    if ( state == ' ' )
+    {
+        state = s;
+    }
+    else if ( s == 'A' )
+    {
+        if ( state == 'C' )
+        {
+            state = 'M';
+        }
+        else if ( state == 'G' )
+        {
+            state = 'R';
+        }
+        else if ( state == 'U' )
+        {
+            state = 'W';
+        }
+        else if ( state == 'Y' )
+        {
+            state = 'H';
+        }
+        else if ( state == 'S' )
+        {
+            state = 'V';
+        }
+        else if ( state == 'K' )
+        {
+            state = 'D';
+        }
+        else if ( state == 'B' )
+        {
+            state = 'N';
+        }
+        else
+        {
+            throw RbException() << "Cannot add state '" << symbol << "' to an RNA character with value '" << state << "'!";
+        }
+    }
+    else if ( s == 'C' )
+    {
+        if ( state == 'A' )
+        {
+            state = 'M';
+        }
+        else if ( state == 'G' )
+        {
+            state = 'S';
+        }
+        else if ( state == 'U' )
+        {
+            state = 'Y';
+        }
+        else if ( state == 'W' )
+        {
+            state = 'H';
+        }
+        else if ( state == 'R' )
+        {
+            state = 'V';
+        }
+        else if ( state == 'K' )
+        {
+            state = 'B';
+        }
+        else if ( state == 'D' )
+        {
+            state = 'N';
+        }
+        else
+        {
+            throw RbException() << "Cannot add state '" << symbol << "' to an RNA character with value '" << state << "'!";
+        }
+    }
+    else if ( s == 'G' )
+    {
+        if ( state == 'A' )
+        {
+            state = 'R';
+        }
+        else if ( state == 'C' )
+        {
+            state = 'S';
+        }
+        else if ( state == 'U' )
+        {
+            state = 'K';
+        }
+        else if ( state == 'M' )
+        {
+            state = 'V';
+        }
+        else if ( state == 'W' )
+        {
+            state = 'D';
+        }
+        else if ( state == 'Y' )
+        {
+            state = 'B';
+        }
+        else if ( state == 'H' )
+        {
+            state = 'N';
+        }
+        else
+        {
+            throw RbException() << "Cannot add state '" << symbol << "' to an RNA character with value '" << state << "'!";
+        }
+    }
+    else if ( s == 'U' )
+    {
+        if ( state == 'A' )
+        {
+            state = 'W';
+        }
+        else if ( state == 'C' )
+        {
+            state = 'Y';
+        }
+        else if ( state == 'G' )
+        {
+            state = 'K';
+        }
+        else if ( state == 'M' )
+        {
+            state = 'H';
+        }
+        else if ( state == 'R' )
+        {
+            state = 'D';
+        }
+        else if ( state == 'S' )
+        {
+            state = 'B';
+        }
+        else if ( state == 'V' )
+        {
+            state = 'N';
+        }
+        else
+        {
+            throw RbException() << "Cannot add state '" << symbol << "' to an RNA character with value '" << state << "'!";
+        }
+    }
+    else
+    {
+        throw RbException() << "Cannot add state '" << symbol << "' to an RNA character with value '" << state << "'!";
+    }
+}
+
 
 std::string RnaState::getDataType( void ) const
 {
@@ -44,38 +231,9 @@ std::string RnaState::getDataType( void ) const
 }
 
 
-std::string RnaState::getStateLabels( void ) const
-{
-    
-    static std::string labels = "ACGU";
-    
-    return labels;
-}
-
-std::string RnaState::getStringValue(void) const
-{
-    
-    return std::string(1,state);
-}
-
-
-void RnaState::addState(const std::string &symbol)
-{
-    //    ++num_observed_states;
-    //
-    //    std::string labels = getStateLabels();
-    //    size_t pos = labels.find(symbol);
-    //
-    //    state.set( pos );
-    //    index_single_state = pos;
-    
-    throw RbException("Cannot add a state to a RNA character!");
-    
-}
-
-
 size_t RnaState::getNumberOfStates(void) const
 {
+    
     return 4;
 }
 
@@ -164,6 +322,51 @@ RbBitSet RnaState::getState(void) const
 }
 
 
+size_t RnaState::getStateIndex(void) const
+{
+    switch ( state )
+    {
+    case 'A': return 0;
+    case 'C': return 1;
+    case 'G': return 2;
+    case 'U': return 3;
+    }
+
+    throw RbException("Cannot get the index of an ambiguous state.");
+}
+
+
+std::string RnaState::getStateLabels( void ) const
+{
+    
+    static std::string labels = "ACGU";
+    
+    return labels;
+}
+
+
+std::string RnaState::getStringValue(void) const
+{
+    if ( isMissingState() )
+    {
+        return "?";
+    }
+    
+    if ( isGapState() )
+    {
+        return "-";
+    }
+    
+    return std::string(1, state);
+}
+
+
+bool RnaState::isAmbiguous(void) const
+{
+    return not (state == 'A' or state == 'G' or state == 'C' or state == 'U');
+}
+
+
 bool RnaState::isGapState( void ) const
 {
     return state == '-';
@@ -198,9 +401,10 @@ void RnaState::setMissingState( bool tf )
 }
 
 
-void RnaState::setToFirstState(void)
+void RnaState::setState(const std::string &symbol)
 {
-    state = 'A';
+    char s = char( toupper( symbol[0] ) );
+    state = s;
 }
 
 
@@ -227,3 +431,8 @@ void RnaState::setStateByIndex(size_t index)
     }
 }
 
+
+void RnaState::setToFirstState(void)
+{
+    state = 'A';
+}
