7a0fb5fba7dd8325c22115e108ecd9c0ad66112c
davidcerny <david.cerny1@gmail.com>
2026-09-14 11:40:07 +0200

    Smarter rounding to eliminate inter-platform differences
    in fossil age reflection

--- src/core/math/RbMathHelper.h
+++ src/core/math/RbMathHelper.h
@@ -6,11 +6,8 @@
  * @brief Namespace containing statistical functions
  *
  * (c) Copyright 2009- under GPL version 3
- * @date Last modified: $Date: 2012-09-10 17:02:20 +0200 (Mon, 10 Sep 2012) $
  * @author The RevBayes Development Core Team
  * @license GPL version 3
- *
- * $Id: RbMathHelper.h 1819 2012-09-10 15:02:20Z hoehna $
  */
 #ifndef RbMathHelper_H
 #define RbMathHelper_H
@@ -22,6 +19,7 @@ namespace RevBayesCore {
         namespace Helper {
             double          fmax2(double x, double y);
             double          fmin2(double x, double y);
+            double          reflectIntoInterval(double x, double lo, double hi);    //!< Bounce x into [lo, hi] by reflection
         }
         
 	}
